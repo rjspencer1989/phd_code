2797c81f39c3551f225849276666da2a9f09a845
Robert Spencer <signup@robspencer.me.uk>
2015-04-30 16:22:28 +0100

    jst compilation

--- ui/_attachments/Gruntfile.js
+++ ui/_attachments/Gruntfile.js
@@ -2,8 +2,17 @@ module.exports = function(grunt){
     grunt.initConfig({
         jshint: {
             files: ['Gruntfile.js', 'script/*.js', 'spec/javascripts/*.js']
+        },
+
+        jst: {
+            compile: {
+                files : {
+                    "./templates.js": ["templates/*.html"]
+                }
+            }
         }
     });
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.registerTask('default', ['jshint']);
+    grunt.loadNpmTasks('grunt-contrib-jst');
+    grunt.registerTask('default', ['jshint', 'jst']);
 };
