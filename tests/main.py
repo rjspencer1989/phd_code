d14bc322175413d8837a99858f193f6138318e0a
Robert Spencer <signup@robspencer.me.uk>
2015-05-10 20:07:16 +0100

    sample test

--- tests/main.py
+++ tests/main.py
@@ -1,1 +1,11 @@
-import pytest
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+    
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('foo'.isupper())
+        
+if __name__ == '__main__':
+    unittest.main()
