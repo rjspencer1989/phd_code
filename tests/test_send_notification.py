336b1277470470c8fc30a759adab05acd101f2bb
Robert Spencer <signup@robspencer.me.uk>
2015-05-11 15:52:57 +0100

    make sure new doc is retrieved correctly

--- tests/test_send_notification.py
+++ tests/test_send_notification.py
@@ -1,9 +1,12 @@
 import unittest
 from couchdbkit import *
-from process_config import ChangeNotification
+from process_config import ChangeNotification, CouchdbConfigParser
 
 
 class TestSendNotification(unittest.TestCase):
     def test_send_notification(self):
         res = ChangeNotification.sendNotification("Rob", "email", "foo")
-        self.assertTrue(res is not None)
+        db = CouchdbConfigParser.getDB()
+        ret_doc = db.get(res)
+        print ret_doc
+        self.assertTrue(ret_doc is not None)
