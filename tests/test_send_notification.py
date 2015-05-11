d9f6a9b37a2e969feeaa287814a937de27bf71e8
Robert Spencer <signup@robspencer.me.uk>
2015-05-11 16:02:01 +0100

    need to get id from return

--- tests/test_send_notification.py
+++ tests/test_send_notification.py
@@ -5,8 +5,10 @@ from process_config import ChangeNotification, CouchdbConfigParser
 
 class TestSendNotification(unittest.TestCase):
     def test_send_notification(self):
+        the_id = ''
         res = ChangeNotification.sendNotification("Rob", "email", "foo")
+        the_id = res['id']
         db = CouchdbConfigParser.getDB()
-        ret_doc = db.get(res)
+        ret_doc = db.get(the_id)
         print ret_doc
         self.assertTrue(ret_doc is not None)
