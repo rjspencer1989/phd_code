ee8317c112f4f30362db61e665ab64f71435cc70
Robert Spencer <signup@robspencer.me.uk>
2015-05-11 12:11:41 +0100

    pep8

--- tests/main.py
+++ tests/main.py
@@ -2,10 +2,8 @@ import unittest
 
 
 class TestStringMethods(unittest.TestCase):
-    
     def test_isupper(self):
         self.assertTrue('FOO'.isupper())
         self.assertFalse('foo'.isupper())
-        
 if __name__ == '__main__':
     unittest.main()
