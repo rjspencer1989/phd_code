70ccde530439f4f47685fe072024a281bfa0d54f
Robert Spencer <signup@robspencer.me.uk>
2015-05-12 12:13:20 +0100

    make sure doc is created

--- tests/test_notification_registration_client.py
+++ tests/test_notification_registration_client.py
@@ -5,5 +5,15 @@ from process_config import CouchdbConfigParser, NotificationRegistrationClient
 
 class TestNotificationRegistrationClient(unittest.TestCase):
     def test_registration(self):
-        name = "Rob"
-        service = "twitter"
+        doc ={
+            name : "Rob",
+            service : "twitter",
+            user : "rjspencer1989",
+            collection: "notifications",
+            status: "pending"
+        }
+
+        db = CouchdbConfigParser.getDB()
+        res = db.save_doc(doc)
+        the_id = res['id']
+        assertNotNone(the_rev)
