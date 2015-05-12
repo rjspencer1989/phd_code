788fac613bc13daecbae2ddec7ac14f0610ae87e
Robert Spencer <signup@robspencer.me.uk>
2015-05-12 15:50:12 +0100

    test hostapd conf lines

--- process_config/WiFi.py
+++ process_config/WiFi.py
@@ -6,6 +6,7 @@ import CouchdbConfigParser
 from os.path import expanduser
 import subprocess
 import ChangeNotification
+import os
 
 db = CouchdbConfigParser.getDB()
 db_info = db.info()
@@ -41,46 +42,52 @@ class WifiProcessor(threading.Thread):
                     timestr = datetime.now().strftime("%H:%M:%S")
                     ChangeNotification.sendNotification(to, service, "network settings updated at %s" % (timestr))
 
+    def generate_config(self, current_doc):
+        line_list = []
+        line_list.append('interface=wlan0\n')
+        line_list.append('driver=nl80211\n')
+        line_list.append('ctrl_interface=/var/run/hostapd\n')
+        line_list.append('ctrl_interface_group=0\n')
+        line_list.append('hw_mode=g\n')
+        line_list.append('auth_algs=3\n')
+        line_list.append('channel=%s\n' % current_doc['channel'])
+        line_list.append('eapol_key_index_workaround=0\n')
+        line_list.append('eap_server=0\n')
+        line_list.append('own_ip_addr=127.0.0.1\n')
+        line_list.append('wep_default_key=0\n')
+        line_list.append('ignore_broadcast_ssid=0\n')
+        line_list.append('ssid=%s\n' % (current_doc['ssid']))
+        if current_doc['password_type'] == 'hex':
+            line_list.append('wep_key0=%s\n' % (current_doc['password']))
+        else:
+            line_list.append('wep_key0=\"%s\"\n' % (current_doc['password']))
+        return line_list
+
     def run(self):
         while(True):
             change = self.shared_object.get()
             the_id = change['id']
             the_rev = change['changes'][0]['rev']
             current_doc = db.open_doc(the_id, rev=the_rev)
-            line_list = []
-            line_list.append('interface=wlan0\n')
-            line_list.append('driver=nl80211\n')
-            line_list.append('ctrl_interface=/var/run/hostapd\n')
-            line_list.append('ctrl_interface_group=0\n')
-            line_list.append('hw_mode=g\n')
-            line_list.append('auth_algs=3\n')
-            line_list.append('channel=%s\n' % current_doc['channel'])
-            line_list.append('eapol_key_index_workaround=0\n')
-            line_list.append('eap_server=0\n')
-            line_list.append('own_ip_addr=127.0.0.1\n')
-            line_list.append('wep_default_key=0\n')
-            line_list.append('ignore_broadcast_ssid=0\n')
-            line_list.append('ssid=%s\n' % (current_doc['ssid']))
-            if current_doc['password_type'] == 'hex':
-                line_list.append('wep_key0=%s\n' % (current_doc['password']))
-            else:
-                line_list.append('wep_key0=\"%s\"\n' % (current_doc['password']))
-            with open('/etc/hostapd/hostapd.conf', 'w') as fh:
-                fh.writelines(line_list)
-            self.get_connected_devices()
-            cmd = ['/etc/init.d/hostapd', 'reload']
-            res = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
-            try:
-                if res.index('done') != -1:
-                    current_doc['status'] = 'done'
-                    self.notify()
-            except ValueError:
-                current_doc['status'] = 'error'
-            finally:
-                db.save_doc(current_doc)
-                self.shared_object.task_done()
+            line_list = self.generate_config(current_doc)
+            if 'ENV_TESTS' not in os.environ:
+                with open('/etc/hostapd/hostapd.conf', 'w') as fh:
+                    fh.writelines(line_list)
+                self.get_connected_devices()
+                cmd = ['/etc/init.d/hostapd', 'reload']
+                res = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+                try:
+                    if res.index('done') != -1:
+                        current_doc['status'] = 'done'
+                        self.notify()
+                except ValueError:
+                    current_doc['status'] = 'error'
+                finally:
+                    db.save_doc(current_doc)
+                    self.shared_object.task_done()
 changeQueue = Queue()
 producer = WifiListener("producer", changeQueue)
 consumer = WifiProcessor("consumer", changeQueue)
-producer.start()
-consumer.start()
+if 'ENV_TESTS' not in os.environ:
+    producer.start()
+    consumer.start()
---
+++ tests/test_wifi.py
@@ -0,0 +1,37 @@
+import unittest
+from process_config import WiFi
+
+class TestWifi(unittest.TestCase):
+    def setUp(self):
+        self.current_doc = {
+            "ssid": "test",
+            "encryption_type": "wep",
+            "mode": "g",
+            "channel": 1
+        }
+
+        self.expected_line_list = [
+            'interface=wlan0\n',
+            'driver=nl80211\n',
+            'ctrl_interface=/var/run/hostapd\n',
+            'ctrl_interface_group=0\n',
+            'hw_mode=g\n',
+            'auth_algs=3\n',
+            'channel=1\n',
+            'eapol_key_index_workaround=0\n',
+            'eap_server=0\n',
+            'own_ip_addr=127.0.0.1\n',
+            'ignore_broadcast_ssid=0\n',
+            'ssid=test\n'
+        ]
+
+    def tearDown(self):
+        self.current_doc = {}
+        self.expected_line_list = []
+
+    def testWifiTextPassword(self):
+        self.current_doc['password_type'] = 'txt'
+        self.current_doc['password'] = 'whatever12345'
+        self.expected_line_list.append('wep_key0=\"whatever12345\"\n')
+        retVal = Wifi.consumer.generate_config(self.current_doc)
+        assertListEqual(self.expected_line_list, retVal)
