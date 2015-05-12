c93eeed6eb9e4b45e4d3e9c1a52e7d8a993abb97
Robert Spencer <signup@robspencer.me.uk>
2015-05-12 13:28:35 +0100

    refactored tests

--- tests/test_notification_registration_client.py
+++ tests/test_notification_registration_client.py
@@ -5,7 +5,8 @@ import os
 
 
 class TestNotificationRegistrationClient(unittest.TestCase):
-    def test_registration(self):
+    ret_doc = {}
+    def setUp():
         doc = {
             "name" : "Rob",
             "service" : "twitter",
@@ -18,8 +19,15 @@ class TestNotificationRegistrationClient(unittest.TestCase):
         res = db.save_doc(doc)
         the_id = res['id']
         ret_doc = db.get(the_id)
+
+    def tearDown(self):
+        ret_doc = {}
+
+    def test_registration(self):
         self.assertIsNotNone(ret_doc)
         NotificationRegistrationClient.consumer.registration(ret_doc, os.environ['APP_ENGINE_ROUTER_ID'])
         added_doc = db.get(the_id)
         print added_doc
         assert('suid' in added_doc)
+
+    def test_edit(self):
