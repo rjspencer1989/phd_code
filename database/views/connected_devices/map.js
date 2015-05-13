51c4b2570696c8f90427f35477af676050d1b43c
Robert Spencer <signup@robspencer.me.uk>
2015-05-13 16:05:48 +0100

    exactly equal
    [ci-skip]

--- database/views/connected_devices/map.js
+++ database/views/connected_devices/map.js
@@ -1,5 +1,5 @@
 function(doc){
-    if(doc.collection === "devices" && doc.connected == true){
+    if(doc.collection === "devices" && doc.connected === true){
         emit(doc.mac_address, doc);
     }
 }
