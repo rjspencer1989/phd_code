cd896f2f4839c7d9354f8a19570d94840bf38553
Robert Spencer <signup@robspencer.me.uk>
2015-05-14 12:56:31 +0100

    cache the id

--- tests/test_notification_registration_client.py
+++ tests/test_notification_registration_client.py
@@ -16,34 +16,34 @@ class TestNotificationRegistrationClient(unittest.TestCase):
 
         self.db = CouchdbConfigParser.getDB()
         res = self.db.save_doc(doc)
-        the_id = res['id']
+        self.the_id = res['id']
         self.ret_doc = self.db.get(the_id)
 
     def tearDown(self):
-        self.db.delete_doc(self.ret_doc._id)
+        self.db.delete_doc(self.the_id)
         self.ret_doc = {}
         self.db = None
 
     def test_registration(self):
         self.assertIsNotNone(self.ret_doc)
         NotificationRegistrationClient.consumer.registration(self.ret_doc, os.environ['APP_ENGINE_ROUTER_ID'])
-        added_doc = self.db.get(self.ret_doc['_id'])
+        added_doc = self.db.get(self.the_id)
         assert('suid' in added_doc)
 
     def test_edit(self):
         self.assertIsNotNone(self.ret_doc)
         NotificationRegistrationClient.consumer.registration(self.ret_doc, os.environ['APP_ENGINE_ROUTER_ID'])
-        added_doc = self.db.get(self.ret_doc['_id'])
+        added_doc = self.db.get(self.the_id)
         added_doc['user'] = 'robjspencer'
         res = self.db.save_doc(added_doc)
-        v2 = self.db.get(res['id'])
+        v2 = self.db.get(self.the_id)
         NotificationRegistrationClient.consumer.edit(v2, os.environ['APP_ENGINE_ROUTER_ID'])
-        added_doc = self.db.get(v2['_id'])
+        added_doc = self.db.get(self.the_id)
         assert('suid' in added_doc)
 
     def test_delete(self):
         self.assertIsNotNone(self.ret_doc)
         NotificationRegistrationClient.consumer.registration(self.ret_doc, os.environ['APP_ENGINE_ROUTER_ID'])
-        added_doc = self.db.get(self.ret_doc['_id'])
+        added_doc = self.db.get(self.the_id)
         code = NotificationRegistrationClient.consumer.delete(added_doc, os.environ['APP_ENGINE_ROUTER_ID'])
         self.assertEqual(code, 200)
