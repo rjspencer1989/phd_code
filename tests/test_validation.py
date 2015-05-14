b94f0770a9ec961264fabf4ddc4b3efd14b92d2a
Robert Spencer <signup@robspencer.me.uk>
2015-05-14 13:36:55 +0100

    proper json

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -4,6 +4,6 @@ from process_config import CouchdbConfigParser
 
 class TestValidation(unittest.TestCase):
     def test_no_collection(self):
-        doc = {foo: "bar"}
+        doc = {"foo": "bar"}
         db = CouchdbConfigParser.getDB()
         db.save_doc(doc)
