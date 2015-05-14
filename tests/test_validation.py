6c41b13693f749912a8118f13ab4a6fac03af1b7
Robert Spencer <signup@robspencer.me.uk>
2015-05-14 15:08:48 +0100

    assert raises doesn't work without with

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -7,9 +7,11 @@ class TestValidation(unittest.TestCase):
     def test_no_collection(self):
         doc = {"foo": "bar"}
         db = CouchdbConfigParser.getDB()
-        self.assertRaises(Exception, db.save_doc(doc))
+        with self.assertRaises(Exception):
+            db.save_doc(doc)
 
     def test_invalid_collection(self):
         doc = {"name":"Rob", "service":"email", "userdetails":"rob@robspencer.me.uk", "collection": "notify"}
         db = CouchdbConfigParser.getDB()
-        self.assertRaises(Exception, db.save_doc(doc))
+        with self.assertRaises(Exception):
+            db.save_doc(doc)
