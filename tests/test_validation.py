60d270ff86b8614e4432a990d95a045b99d8f361
Robert Spencer <signup@robspencer.me.uk>
2015-05-14 13:55:37 +0100

    see if that makes a difference

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,9 +1,11 @@
 import unittest
 from process_config import CouchdbConfigParser
+import urllib2
 
 
 class TestValidation(unittest.TestCase):
     def test_no_collection(self):
         doc = {"foo": "bar"}
         db = CouchdbConfigParser.getDB()
-        self.assertRaises("Unauthorized", db.save_doc(doc))
+        with self.assertRaises(urllib2.HTTPError):
+            db.save_doc(doc)
