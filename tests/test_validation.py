122110dbfc1b5baa8fbbf7d37d222931ba96f7b2
Robert Spencer <signup@robspencer.me.uk>
2015-05-14 15:06:17 +0100

    test invalid collection

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -7,5 +7,9 @@ class TestValidation(unittest.TestCase):
     def test_no_collection(self):
         doc = {"foo": "bar"}
         db = CouchdbConfigParser.getDB()
-        with self.assertRaises(Exception):
-            db.save_doc(doc)
+        self.assertRaises(Exception, db.save_doc(doc))
+
+    def test_invalid_collection(self):
+        doc = {"name":"Rob", "service":"email", "userdetails":"rob@robspencer.me.uk", "collection": "notify"}
+        db = CouchdbConfigParser.getDB()
+        self.assertRaises(Exception, db.save_doc(doc))
