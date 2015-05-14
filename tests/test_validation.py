2729234f7d0903f508d9e409a856466649da7af0
Robert Spencer <signup@robspencer.me.uk>
2015-05-14 14:11:02 +0100

    try and find exception

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,11 +1,11 @@
 import unittest
 from process_config import CouchdbConfigParser
-import urllib2
+from couchdbkit import *
 
 
 class TestValidation(unittest.TestCase):
     def test_no_collection(self):
         doc = {"foo": "bar"}
         db = CouchdbConfigParser.getDB()
-        with self.assertRaises(urllib2.HTTPError):
+        with self.assertRaises(couchdbkit.BadValueError):
             db.save_doc(doc)
