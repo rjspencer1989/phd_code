f164947b8cfad9743ff61e18376552be8cb8b433
Robert Spencer <signup@robspencer.me.uk>
2015-05-14 15:21:53 +0100

    tests of various types

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,6 +1,5 @@
 import unittest
 from process_config import CouchdbConfigParser
-import couchdbkit.exceptions
 
 
 class TestValidation(unittest.TestCase):
@@ -11,7 +10,46 @@ class TestValidation(unittest.TestCase):
             db.save_doc(doc)
 
     def test_invalid_collection(self):
-        doc = {"name":"Rob", "service":"email", "userdetails":"rob@robspencer.me.uk", "collection": "notify"}
+        doc = {
+            "name":"Rob",
+            "service":"email",
+            "userdetails":"rob@robspencer.me.uk",
+            "collection": "notify"
+        }
+        db = CouchdbConfigParser.getDB()
+        with self.assertRaises(Exception):
+            db.save_doc(doc)
+
+    def test_notification_valid(self):
+        doc = {
+            "name" : "Rob",
+            "service" : "twitter",
+            "collection": "notifications",
+            "user": "rjspencer1989",
+            "status": "pending"
+        }
+        db = CouchdbConfigParser.getDB()
+        db.save_doc(doc)
+
+    def test_notification_invalid_status(self):
+        doc = {
+            "name" : "Rob",
+            "service" : "twitter",
+            "collection": "notifications",
+            "user": "rjspencer1989",
+            "status": "foo"
+        }
+        db = CouchdbConfigParser.getDB()
+        with self.assertRaises(Exception):
+            db.save_doc(doc)
+
+    def test_notification_missing_name(self):
+        doc = {
+            "service" : "twitter",
+            "collection": "notifications",
+            "user": "rjspencer1989",
+            "status": "foo"
+        }
         db = CouchdbConfigParser.getDB()
         with self.assertRaises(Exception):
             db.save_doc(doc)
