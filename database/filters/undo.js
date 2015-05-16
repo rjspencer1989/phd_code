4ade4e1563619e4edb6aef0ffb2871ccf6392d02
Robert Spencer <signup@robspencer.me.uk>
2015-05-16 14:31:02 +0100

    reinstate full filter

--- database/filters/undo.js
+++ database/filters/undo.js
@@ -1,5 +1,5 @@
 function (doc, req){
-    if(doc.collection === "events" /*&& doc.perform_undo === true && doc.undoable === true*/){
+    if(doc.collection === "events" && doc.perform_undo === true && doc.undoable === true){
         return true;
     }
     return false;
