e3d83913bed2b93076f63344e457d64a3dd3bd00
Robert Spencer <signup@robspencer.me.uk>
2015-05-16 18:13:56 +0100

    initial view test

---
+++ tests/test_views.py
@@ -0,0 +1,26 @@
+import unittest
+from process_config import CouchdbConfigParser
+from couchdbkit import *
+
+
+class TestViews(unittest.TestCase):
+    def test_notification_with_service(self):
+        db = CouchdbConfigParser.getDB()
+        doc1 = {
+            "collection": "notifications",
+            "name": "Rob",
+            "service": "email",
+            "user": "rob@robspencer.me.uk",
+            "status": "done"
+        }
+        
+        doc2 = {
+            "collection": "notifications",
+            "name": "Rob",
+            "service": "growl",
+            "user": "10.2.0.1",
+            "status": "done"
+        }
+        
+        res = db.save_doc(doc1)
+        res2 = db.save_doc(doc2)
