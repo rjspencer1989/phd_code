298971bd2c175a2fcf981db87d77cb229f0d1290
Robert Spencer <signup@robspencer.me.uk>
2015-05-17 15:58:04 +0100

    more view tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -139,3 +139,128 @@ class TestViews(unittest.TestCase):
         self.assertEqual(len(l_vra), 1)
         self.assertEqual(l_vra[0]['id'], res['id'])
         db.delete_doc(res['id'])
+
+    def test_dhcp(self):
+        doc1 = {
+           "action":"",
+           "device_name": "psxrjs-mbp",
+           "host_name": "psxrjs-mbp",
+           "ip_address": "10.2.0.1",
+           "mac_address" : "68:a8:6d:3b:05:e4",
+           "name": "Rob",
+           "state": "permit",
+           "timestamp": time.time(),
+           "collection": "devices",
+           "lease_action": "add",
+           "device_type": "laptop",
+           "notification_service": "email"
+        }
+        
+        db = CouchdbConfigParser.getDB()
+        res = db.save_doc(doc)
+        vr = db.view("homework-remote/dhcp")
+        vra = vr.all()
+        l_vra = list(vra)
+        self.assertEqual(len(l_vra), 1)
+        self.assertEqual(l_vra[0]['id'], res['id'])
+        self.assertEqual(l_vra[0]['key'], "68:a8:6d:3b:05:e4")
+        db.delete_doc(res['id'])
+
+    def test_control(self):
+        doc1 = {
+           "action":"",
+           "device_name": "psxrjs-mbp",
+           "host_name": "psxrjs-mbp",
+           "ip_address": "10.2.0.1",
+           "mac_address" : "68:a8:6d:3b:05:e4",
+           "name": "Rob",
+           "state": "permit",
+           "timestamp": time.time(),
+           "collection": "devices",
+           "lease_action": "add",
+           "device_type": "laptop",
+           "notification_service": "email"
+        }
+        
+        db = CouchdbConfigParser.getDB()
+        res = db.save_doc(doc)
+        vr = db.view("homework-remote/control")
+        vra = vr.all()
+        l_vra = list(vra)
+        self.assertEqual(len(l_vra), 1)
+        self.assertEqual(l_vra[0]['id'], res['id'])
+        self.assertEqual(l_vra[0]['key'], True)
+        db.delete_doc(res['id'])
+
+    def test_device_notification_service_mapping(self):
+        doc1 = {
+           "action":"",
+           "device_name": "psxrjs-mbp",
+           "host_name": "psxrjs-mbp",
+           "ip_address": "10.2.0.1",
+           "mac_address" : "68:a8:6d:3b:05:e4",
+           "name": "Rob",
+           "state": "permit",
+           "timestamp": time.time(),
+           "collection": "devices",
+           "lease_action": "add",
+           "device_type": "laptop",
+           "notification_service": "email"
+        }
+        
+        db = CouchdbConfigParser.getDB()
+        res = db.save_doc(doc)
+        vr = db.view("homework-remote/device_notification_service_mapping")
+        vra = vr.all()
+        l_vra = list(vra)
+        self.assertEqual(len(l_vra), 1)
+        self.assertEqual(l_vra[0]['id'], res['id'])
+        self.assertEqual(l_vra[0]['key'], "68:a8:6d:3b:05:e4")
+        self.assertEqual(l_vra[0]['value']['name'], "Rob")
+        self.assertEqual(l_vra[0]['value']['service'], "email")
+        db.delete_doc(res['id'])
+
+    def test_connected_devices(self):
+        doc1 = {
+           "action":"",
+           "device_name": "psxrjs-mbp",
+           "host_name": "psxrjs-mbp",
+           "ip_address": "10.2.0.1",
+           "mac_address" : "68:a8:6d:3b:05:e4",
+           "name": "Rob",
+           "state": "permit",
+           "timestamp": time.time(),
+           "collection": "devices",
+           "lease_action": "add",
+           "device_type": "laptop",
+           "notification_service": "email",
+           "connected": True
+        }
+        
+        doc2 = {
+           "action":"",
+           "device_name": "psxrjs-mbp-eth",
+           "host_name": "psxrjs-mbp",
+           "ip_address": "10.2.0.4",
+           "mac_address" : "68:a8:6d:3b:05:e5",
+           "name": "Rob",
+           "state": "permit",
+           "timestamp": time.time(),
+           "collection": "devices",
+           "lease_action": "del",
+           "device_type": "laptop",
+           "notification_service": "email",
+           "connected": False
+        }
+        
+        db = CouchdbConfigParser.getDB()
+        res = db.save_doc(doc)
+        res2 = db.save_doc(doc2)
+        vr = db.view("homework-remote/connected_devices")
+        vra = vr.all()
+        l_vra = list(vra)
+        self.assertEqual(len(l_vra), 1)
+        self.assertEqual(l_vra[0]['id'], res['id'])
+        self.assertEqual(l_vra[0]['key'], "68:a8:6d:3b:05:e4")
+        db.delete_doc(res['id'])
+        db.delete_doc(res2['id'])
