daea788d2550fcadbb567277aaba78eaeac73061
Robert Spencer <signup@robspencer.me.uk>
2015-05-17 22:26:15 +0100

    starting test code for undo and rollback that will be skipped need to refactor code for them to run

--- tests/test_process_rollback.py
+++ tests/test_process_rollback.py
@@ -1,1 +1,11 @@
 import unittest
+from process_config import CouchdbConfigParser, Rollback
+import time
+
+
+class TestProcessRollback(unittest.TestCase):
+    @unittest.skip("need to create implementation classes")
+    def test_process_rollback(self):
+        timestamp = time.time() - 20
+        result = Rollback.perform_rollback(timestamp)
+        self.assertEqual(result, True)
--- tests/test_process_undo.py
+++ tests/test_process_undo.py
@@ -1,1 +1,12 @@
 import unittest
+from process_config import CouchdbConfigParser, Undo, History
+
+
+class TestProcessUndo(unittest.TestCase):
+    @unittest.skip("need to remove auto thread start for tests")
+    def test_process_undo(self):
+        undo_processor = Undo.UndoProcessor()
+        event = {}
+        doc = undo_processor.get_doc_to_undo(event)
+        rev_list = undo_processor.get_rev_list(doc)
+        result = undo_processor.undo(rev_list)
