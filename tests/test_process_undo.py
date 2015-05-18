97592e41299b7e3e5c052d3e06467209f8c9492e
Robert Spencer <signup@robspencer.me.uk>
2015-05-18 11:33:24 +0100

    use existing thread

--- tests/test_process_undo.py
+++ tests/test_process_undo.py
@@ -4,8 +4,8 @@ from process_config import CouchdbConfigParser, Undo, History
 
 class TestProcessUndo(unittest.TestCase):
     def test_process_undo(self):
-        undo_processor = Undo.UndoProcessor()
+        undo_consumer = Undo.consumer
         event = {}
-        doc = undo_processor.get_doc_to_undo(event)
-        rev_list = undo_processor.get_rev_list(doc)
-        result = undo_processor.undo(rev_list)
+        doc = undo_consumer.get_doc_to_undo(event)
+        rev_list = undo_consumer.get_rev_list(doc)
+        result = undo_consumer.undo(rev_list)
