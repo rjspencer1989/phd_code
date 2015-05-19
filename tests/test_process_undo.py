2c5df691568ddae0b43060526444cc83c2297e8b
Robert Spencer <signup@robspencer.me.uk>
2015-05-19 10:37:04 +0100

    test undo multiple revisions

--- tests/test_process_undo.py
+++ tests/test_process_undo.py
@@ -23,3 +23,25 @@ class TestProcessUndo(unittest.TestCase):
         result = undo_consumer.undo(doc, rev_list)
         print db.get(doc['_id'], rev=result)
         db.delete_doc(event_res['id'])
+
+    def test_process_undo_existing_doc(self):
+        undo_consumer = Undo.consumer
+        nd = {
+            "collection": "notifications",
+            "status": "done",
+            "name": "Rob",
+            "service": "twitter",
+            "user": "robjspencer"
+        }
+        db = CouchdbConfigParser.getDB()
+        res = db.save_doc(nd)
+        res2 = db.save_doc(nd)
+        event_res = History.add_history_item("edit notification", "Edited notification mapping for Rob using Twitter and username robjspencer", "Rob", res2['id'], res2['rev'], True)
+        event = db.get(event_res['id'])
+        doc = undo_consumer.get_doc_to_undo(event)
+        rev_list = undo_consumer.get_rev_list(doc, res['rev'])
+        print rev_list
+        result = undo_consumer.undo(doc, rev_list)
+        print db.get(doc['_id'], rev=result)
+        db.delete_doc(res['id'])
+        db.delete_doc(event_res['id'])
