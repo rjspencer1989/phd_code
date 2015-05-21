403be0ea07b4b1fd0b4fc51318d8df15eb9eebf7
Robert Spencer <signup@robspencer.me.uk>
2015-05-21 12:12:57 +0100

    complete rewrite of notifications

--- notifications/notification_service_runner.py
+++ notifications/notification_service_runner.py
@@ -26,53 +26,44 @@ class NotificationRequestProcessor(threading.Thread):
         super(NotificationRequestProcessor, self).__init__(name=threadName)
         self.sharedObject = queue
 
-    def send_notification(self, notification_id, name, service, rows, message):
-        success = False
-        if len(rows) > 0:
-            for row in rows:
-                user_details = row['value']
-                encoded = service.encode('utf8')
-                mod = __import__('NotificationServices', fromlist=[encoded])
-                service_class = getattr(mod, service)
-                result = service_class.sendNotification(notification_id, user_details, message)
-                success = result
-        return success
+    def send_notification(self, notification_id, name, service, user, message):
+        encoded = service.encode('utf8')
+        mod = __import__('NotificationServices', fromlist=[encoded])
+        service_class = getattr(mod, service)
+        result = service_class.sendNotification(notification_id, user_details, message)
+        return result
 
-    def process_notification(self, doc):
-        name_list = []
-        if doc['to'].lower() == 'everyone':
-            name_list = self.get_registered_names()
+    def get_user_names(self, name, service):
+        if name.lower() == 'everyone':
+            start_key = [service, ""]
+            end_key = [service, {}]
         else:
-            name_list = [doc['to']]
-        if len(name_list) > 0:
-            for name in name_list:
-                service = doc['service'].lower()
-                service_res_all = self.get_user_name(name, service)
-                if service_res_all is not None:
-                    ret = self.send_notification(doc['id'], name, service, service_res_all, doc['body'])
-                    if ret:
-                        doc['status'] = "done"
-                    else:
-                        doc['status'] = "error"
-        else:
-            doc['status'] = 'error'
-        db.save_doc(doc)
-
-    def get_registered_names(self):
-        res = db.view('homework-remote/notification_names', group=True)
-        res_all = res.all()
-        name_list = []
-        for row in res_all:
-            name_list.append(row['key'])
-        return name_list
-
-    def get_user_name(self, name, service):
-        key = [name, service]
-        service_res = db.view('homework-remote/notification_with_service', key=key)
+            start_key = [service, name]
+            end_key = [service, name]
+        service_res = db.view('homework-remote/notification_with_service', startkey=start_key, endkey=end_key)
         service_res_all = serviceRes.all()
         if len(service_res_all) > 0:
             return service_res_all
         return None
+
+    def process_notification(self, doc):
+        usernames = self.get_user_names(doc['to'], doc['service'])
+        ret = False
+        if usernames is not None:
+            for result in usernames:
+                service = doc['service'].lower()
+                name = doc['to']
+                if name.lower() == 'everyone':
+                    name = result['key'][1]
+                user = result['value']
+                ret = self.send_notification(doc['_id'], name, service, user, doc['body'])
+            if ret:
+                doc['status'] = "done"
+            else:
+                doc['status'] = "error"
+        else:
+            doc['status'] = 'error'
+        db.save_doc(doc)
 
     def run(self):
         while True:
