46d7f383db5ef46c4cca103c91e8468544ea6caf
Robert Spencer <signup@robspencer.me.uk>
2015-05-21 15:19:07 +0100

    auto complete in atom puts a self in

--- tests/test_notification_service_runner.py
+++ tests/test_notification_service_runner.py
@@ -4,4 +4,4 @@ from notifications import notification_service_runner
 
 class TestNotificationServiceRunner(unittest.TestCase):
     def test_process_notification(self):
-        self.self.assertEqual(2, 2)
+        self.assertEqual(2, 2)
