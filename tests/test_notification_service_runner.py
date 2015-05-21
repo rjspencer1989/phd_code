9759825ea7ccd6f09093c988ffb65fb8af8ffb41
Robert Spencer <signup@robspencer.me.uk>
2015-05-21 15:13:44 +0100

    dummy test

--- tests/test_notification_service_runner.py
+++ tests/test_notification_service_runner.py
@@ -1,1 +1,7 @@
 import unittest
+from notifications import notification_service_runner
+
+
+class TestNotificationServiceRunner(unittest.TestCase):
+    def test_process_notification(self):
+        self.self.assertEqual(2, 2)
