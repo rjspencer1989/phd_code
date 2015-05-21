a18d8ad4cc0cd67a7d331255bf901a624e67174b
Robert Spencer <signup@robspencer.me.uk>
2015-05-21 15:30:24 +0100

    make all filenames lower case

--- tests/test_notification_service_runner.py
+++ tests/test_notification_service_runner.py
@@ -1,7 +1,13 @@
 import unittest
 from notifications import notification_service_runner
+from process_config import couchdb_config_parser
 
 
 class TestNotificationServiceRunner(unittest.TestCase):
     def test_process_notification(self):
-        self.assertEqual(2, 2)
+        doc = {
+            "body": "test",
+            "collection": "notification-request",
+            "service": "phone",
+
+        }
