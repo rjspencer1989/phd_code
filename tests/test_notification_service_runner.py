860c28a2badddfb7345388d4e12be2fffa5e1293
Robert Spencer <signup@robspencer.me.uk>
2015-05-22 14:06:01 +0100

    test setup and teardown

--- tests/test_notification_service_runner.py
+++ tests/test_notification_service_runner.py
@@ -4,14 +4,48 @@ from process_config import couchdb_config_parser
 
 
 class TestNotificationServiceRunner(unittest.TestCase):
+    @clasmethod
+    def setUpClass(cls):
+        cls.nsr = notification_service_runner.notification_consumer
+        cls.db = couchdb_config_parser.get_db()
+
+    @clasmethod
+    def tearDownClass(cls):
+        cls.nsr = None
+        cls.db = None
+
+    def setUp(self):
+        self.registration1 = {
+            "collection": "notifications",
+            "name": "Rob",
+            "service": "phone",
+            "user": "+447972058628",
+            "status": "done"
+        }
+
+        self.registration2 = {
+            "collection": "notifications",
+            "name": "Harry",
+            "service": "phone",
+            "user": "+447972058628",
+            "status": "done"
+        }
+
+        self.reg_result1 = db.save_doc(self.registration1)
+        self.reg_result2 = db.save_doc(self.registration2)
+
+    def tearDown(self):
+        db.delete_doc(self.reg_result['id'])
+        self.registration1 = {}
+        self.registration2 = {}
+
     def test_get_user_names_no_match(self):
-        nsr = notification_service_runner.notification_consumer
         doc = {
             "body": "test",
             "collection": "notification-request",
             "service": "phone",
             "status": "pending",
-            "to": "Rob"
+            "to": "Tom"
         }
 
         db = couchdb_config_parser.get_db()
