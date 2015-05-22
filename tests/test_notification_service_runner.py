38890d2f40065a3f2d90bcec737c021aa6735074
Robert Spencer <signup@robspencer.me.uk>
2015-05-22 12:11:07 +0100

    get usernames

--- tests/test_notification_service_runner.py
+++ tests/test_notification_service_runner.py
@@ -4,7 +4,8 @@ from process_config import couchdb_config_parser
 
 
 class TestNotificationServiceRunner(unittest.TestCase):
-    def test_process_notification(self):
+    def test_get_user_names(self):
+        nsr = notification_service_runner.notification_consumer
         doc = {
             "body": "test",
             "collection": "notification-request",
@@ -15,4 +16,6 @@ class TestNotificationServiceRunner(unittest.TestCase):
 
         db = couchdb_config_parser.get_db()
         res = db.save_doc(doc)
+        user_names = nsr.get_user_names(doc['to'], doc['service'])
+        print user_names
         db.delete_doc(res['id'])
