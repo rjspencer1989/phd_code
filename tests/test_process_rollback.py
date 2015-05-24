69f270952500f16ee08165372f5367568f9d00d2
Robert Spencer <signup@robspencer.me.uk>
2015-05-24 10:35:32 +0100

    tidy up

--- tests/test_process_rollback.py
+++ tests/test_process_rollback.py
@@ -17,8 +17,8 @@ class TestProcessRollback(unittest.TestCase):
         }
         db = couchdb_config_parser.get_db()
         res1 = db.save_doc(doc1)
-        add_history.add_history_item("edit wifi", "edit wifi", "Rob", res1['id'], res1['rev'], True)
+        hist1 = add_history.add_history_item("edit wifi", "edit wifi", "Rob", res1['id'], res1['rev'], True)
         timestamp = time.time() - 20
         result = perform_rollback.perform_rollback(timestamp)
-        self.assertEqual(result, True)
         db.delete_doc(res1['id'])
+        db.delete_doc(hist1['id'])
