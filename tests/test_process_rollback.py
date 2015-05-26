c6e3b2cd51420e2cb59a58a0b3b29daaf0aabd44
Robert Spencer <signup@robspencer.me.uk>
2015-05-26 10:20:06 +0100

    make it more verbose

--- tests/test_process_rollback.py
+++ tests/test_process_rollback.py
@@ -29,6 +29,7 @@ class TestProcessRollback(unittest.TestCase):
         dt = datetime.datetime(2015, 2, 23, hour=15, minute=0)
         hist3 = self.add_history_item(res3['id'], res3['rev'], dt.isoformat())
         result = perform_rollback.perform_rollback(datetime.datetime(2015, 1, 20).isoformat())
+        print result
         result_list = list(result)
         self.assertEqual(3, len(result_list))
         db.delete_doc(res1['id'])
