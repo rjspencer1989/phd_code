f4abbb20b1199dd23651380ed5a11444c450bf3b
Robert Spencer <signup@robspencer.me.uk>
2015-05-26 10:43:35 +0100

    specify endkey limit

--- process_config/perform_rollback.py
+++ process_config/perform_rollback.py
@@ -4,6 +4,6 @@ from couchdbkit import *
 
 def perform_rollback(timestamp):
     db = couchdb_config_parser.get_db()
-    vr = db.view('homework-remote/events')
+    vr = db.view('homework-remote/events', descending=True, endkey=timestamp)
     vra = vr.all()
     return vra
