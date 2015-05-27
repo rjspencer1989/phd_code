23955d99ebbc2c08163b100a33fc4bbcc18fa27b
Robert Spencer <signup@robspencer.me.uk>
2015-05-27 15:34:50 +0100

    stop spam

--- tests/test_notification_service_runner.py
+++ tests/test_notification_service_runner.py
@@ -91,7 +91,7 @@ class TestNotificationServiceRunner(unittest.TestCase):
         self.assertEqual('Rob', user_names_lst[1]['key'][1])
         self.db.delete_doc(res['id'])
 
-    # @unittest.skip("stop the spam")
+    @unittest.skip("stop the spam")
     def test_send_notification(self):
         doc = {
             "body": "test",
