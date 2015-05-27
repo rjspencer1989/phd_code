22c0627ef25a41443b046d34f6e8180b5b8d47ec
Robert Spencer <signup@robspencer.me.uk>
2015-05-27 15:56:53 +0100

    remove unused test

--- tests/test_views.py
+++ tests/test_views.py
@@ -37,45 +37,6 @@ class TestViews(unittest.TestCase):
         db.delete_doc(res['id'])
         db.delete_doc(res2['id'])
 
-    def test_notification_names(self):
-        db = couchdb_config_parser.get_db()
-        doc1 = {
-            "collection": "notifications",
-            "name": "Rob",
-            "service": "email",
-            "user": "rob@robspencer.me.uk",
-            "status": "done"
-        }
-
-        doc2 = {
-            "collection": "notifications",
-            "name": "Rob",
-            "service": "growl",
-            "user": "10.2.0.1",
-            "status": "done"
-        }
-
-        doc3 = {
-            "collection": "notifications",
-            "name": "Harry",
-            "service": "email",
-            "user": "harrye@robspencer.me.uk",
-            "status": "done"
-        }
-
-        res = db.save_doc(doc1)
-        res2 = db.save_doc(doc2)
-        res3 = db.save_doc(doc3)
-        vr = db.view("homework-remote/notification_names", group=True)
-        vra = vr.all()
-        l_vra = list(vra)
-        self.assertEqual(len(l_vra), 2)
-        self.assertEqual(l_vra[0]['key'], 'Harry')
-        self.assertEqual(l_vra[1]['key'], 'Rob')
-        db.delete_doc(res['id'])
-        db.delete_doc(res2['id'])
-        db.delete_doc(res3['id'])
-
     def test_valid_leases(self):
         doc1 = {
             "action": "",
