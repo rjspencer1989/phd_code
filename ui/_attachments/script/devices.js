eb55100c4cee397c3f060f19e180461d41f75060
Robert Spencer <signup@robspencer.me.uk>
2015-05-28 15:21:41 +0100

    set changed_by in wrong place

--- ui/_attachments/script/devices.js
+++ ui/_attachments/script/devices.js
@@ -101,9 +101,9 @@ App.Views.Device = Backbone.View.extend({
             this.model.set({device_name: device_name});
             this.model.set({device_type: device_type});
             this.model.set({notification_service: notification_service});
-            this.model.set({changed_by: 'user'});
         }
         this.model.set({action: 'permit'});
+        this.model.set({changed_by: 'user'});
         this.model.save(null, {
             success: function(model, response, options){
                 console.log(response);
