a6b167d7dbfdbe1d29563f298dc79352cf4c55e2
Robert Spencer <signup@robspencer.me.uk>
2015-06-02 11:34:11 +0100

    debug

--- ui/_attachments/script/devices.js
+++ ui/_attachments/script/devices.js
@@ -183,6 +183,7 @@ App.Views.ControlPanelView = Backbone.View.extend({
 
     render: function(){
         this.$el.empty().append(this.template());
+        console.log(this.$el);
         $('#main-row').empty().append(this.$el.html());
         setActiveLink('services-link');
         $('.alert').hide();
