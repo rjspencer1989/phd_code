8a35c418cbd0e12075daa059511293c8c466b6ef
Robert Spencer <signup@robspencer.me.uk>
2015-06-04 15:27:36 +0100

    grab logged in username at startup

--- ui/_attachments/script/startup.js
+++ ui/_attachments/script/startup.js
@@ -3,4 +3,10 @@ $(function(){
     var router = new App.Routers.Router();
     App.routerInstance = router;
     Backbone.history.start();
+    $.couch.session({
+        success: function (data) {
+            console.log(data);
+            App.userCtx = data.userCtx;
+        }
+    })
 });
