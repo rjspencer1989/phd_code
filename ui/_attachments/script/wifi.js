99e516d269057097e527f5d2b20aefb039e58b98
Robert Spencer <signup@robspencer.me.uk>
2015-06-05 12:08:58 +0100

    wifi worked first time

--- ui/_attachments/script/wifi.js
+++ ui/_attachments/script/wifi.js
@@ -55,9 +55,9 @@ App.Views.Wifi = Backbone.View.extend({
         if(newPasswordType !== 'blank') mod.set({password_type: newPasswordType});
         if(newPassword !== '') mod.set({password: newPassword});
         mod.set({status: 'pending'});
-        addHistoryEvent("New WiFi Configuration", "WiFi configuration has been updated and devices will need to be reconnected", App.userCtx.name, mod.id, mod.get('_rev'), true);
         mod.save(null, {
             success: function(model, response){
+                addHistoryEvent("New WiFi Configuration", "WiFi configuration has been updated and devices will need to be reconnected", App.userCtx.name, model.id, model.get('_rev'), true);
                 console.log(response);
             },
             error: function(model, response){
