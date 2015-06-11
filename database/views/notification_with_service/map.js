8931e1c67ef0a996f3ff37172b90d3115c00fc9e
Robert Spencer <signup@robspencer.me.uk>
2015-06-11 15:33:35 +0100

    view criteria wrong

--- database/views/notification_with_service/map.js
+++ database/views/notification_with_service/map.js
@@ -1,5 +1,5 @@
 function(doc){
-	if(doc.collection === "notifications" && !doc.hasOwnProperty('hidden')){
+	if(doc.collection === "notifications" && doc.hasOwnProperty('hidden') === false){
 		emit([doc.service, doc.name], doc.user);
 	}
 }
