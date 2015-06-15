d50ec3a899fccb3facad7c508684585a70824df6
Robert Spencer <signup@robspencer.me.uk>
2015-06-15 14:43:08 +0100

    import the new undo system

--- tests/test_process_undo.py
+++ tests/test_process_undo.py
@@ -1,5 +1,6 @@
 import unittest
-from process_config import couchdb_config_parser, perform_undo, add_history
+from process_config import couchdb_config_parser, add_history
+from undo import perform_undo
 import time
 
 
