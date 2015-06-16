5e15429736d7cfff4e0d16c4993e93aa433c6fb5
Robert Spencer <signup@robspencer.me.uk>
2015-06-16 15:19:49 +0100

    test device

--- tests/test_process_undo.py
+++ tests/test_process_undo.py
@@ -88,49 +88,49 @@ class TestPerformUndo(unittest.TestCase):
         self.db.save_doc(nd, force_update=True)
         event['_deleted'] = True
         self.db.save_doc(event, force_update=True)
-    #
-    # def test_process_undo_device_doc(self):
-    #     undo_consumer = perform_undo.consumer
-    #     doc = {
-    #         "_id": "11:aa:33:bb:cc:ff",
-    #         "action": "",
-    #         "collection": "devices",
-    #         "device_name": "",
-    #         "host_name": "test-device",
-    #         "ip_address": "10.2.0.61",
-    #         "lease_action": "add",
-    #         "mac_address": "11:aa:33:bb:cc:ff",
-    #         "name": "",
-    #         "state": "pending",
-    #         "device_type": "",
-    #         "notification_service": "",
-    #         "timestamp": time.time(),
-    #         "connection_event": "connect",
-    #         "changed_by": "system"
-    #     }
-    #     db = couchdb_config_parser.get_db()
-    #     db.save_doc(doc)  # initial from DHCP
-    #     doc['action'] = 'deny'
-    #     doc['device_name'] = 'test-device'
-    #     doc['name'] = 'Rob'
-    #     doc['device_type'] = 'laptop'
-    #     doc['notification_service'] = 'phone'
-    #     doc['changed_by'] = 'user'
-    #     db.save_doc(doc)  # initial user decision
-    #     doc['action'] = ''
-    #     doc['state'] = 'deny'
-    #     doc['changed_by'] = 'system'
-    #     db.save_doc(doc)  # POX applying change
-    #     doc['action'] = 'permit'
-    #     doc['changed_by'] = 'user'
-    #     res = db.save_doc(doc)  # user changes their mind
-    #     event_res = add_history.add_history_item("Permit device", "Permitted  test-device", res['id'], res['rev'], True)
-    #     event = db.get(event_res['id'])
-    #     undo_doc = undo_consumer.get_doc_to_undo(event)
-    #     result = undo_consumer.undo_device_change(undo_doc, res['rev'])
-    #     updated = db.open_doc(doc['_id'], rev=result)
-    #     self.assertEqual('deny', updated['action'])
-    #     event['_deleted'] = True
-    #     doc['_deleted'] = True
-    #     db.save_doc(doc, force_update=True)
-    #     db.save_doc(event, force_update=True)
+
+    def test_process_undo_device_doc(self):
+        undo_consumer = perform_undo.consumer
+        doc = {
+            "_id": "11:aa:33:bb:cc:ff",
+            "action": "",
+            "collection": "devices",
+            "device_name": "",
+            "host_name": "test-device",
+            "ip_address": "10.2.0.61",
+            "lease_action": "add",
+            "mac_address": "11:aa:33:bb:cc:ff",
+            "name": "",
+            "state": "pending",
+            "device_type": "",
+            "notification_service": "",
+            "timestamp": time.time(),
+            "connection_event": "connect",
+            "changed_by": "system"
+        }
+        db = couchdb_config_parser.get_db()
+        db.save_doc(doc)  # initial from DHCP
+        doc['action'] = 'deny'
+        doc['device_name'] = 'test-device'
+        doc['name'] = 'Rob'
+        doc['device_type'] = 'laptop'
+        doc['notification_service'] = 'phone'
+        doc['changed_by'] = 'user'
+        db.save_doc(doc)  # initial user decision
+        doc['action'] = ''
+        doc['state'] = 'deny'
+        doc['changed_by'] = 'system'
+        db.save_doc(doc)  # POX applying change
+        doc['action'] = 'permit'
+        doc['changed_by'] = 'user'
+        res = db.save_doc(doc)  # user changes their mind
+        event_res = add_history.add_history_item("Permit device", "Permitted  test-device", res['id'], res['rev'], True)
+        event = db.get(event_res['id'])
+        undo_doc = undo_consumer.get_doc_to_undo(event)
+        result = undo_consumer.undo_device_change(undo_doc, res['rev'])
+        updated = db.open_doc(doc['_id'], rev=result)
+        self.assertEqual('deny', updated['action'])
+        event['_deleted'] = True
+        doc['_deleted'] = True
+        db.save_doc(doc, force_update=True)
+        db.save_doc(event, force_update=True)
