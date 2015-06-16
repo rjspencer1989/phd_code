684c4a43c58b668c7a90a298b57fe7527e3b30bf
Robert Spencer <signup@robspencer.me.uk>
2015-06-16 15:47:13 +0100

    filter rev list to user changes

--- undo/doc_types/devices.py
+++ undo/doc_types/devices.py
@@ -2,6 +2,14 @@ from base_doc import BaseDoc
 import pprint
 
 class Devices(BaseDoc):
+    def get_rev_list(self):
+        revs=[]
+        initial_revs = super(Devices, self).get_rev_list()
+        for rev in initial_revs:
+            doc = self.db.get(self.doc['_id'], rev=rev)
+            if doc['changed_by'] == 'user':
+                revs.append(rev)
+
     def undo(self):
         rev_list = self.get_rev_list()
         pprint.pprint(rev_list)
