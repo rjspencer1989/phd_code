5c9f9da3577e540340ae82941ba072ad1a80c84c
Robert Spencer <signup@robspencer.me.uk>
2015-06-16 14:43:01 +0100

    debug

--- undo/doc_types/notifications.py
+++ undo/doc_types/notifications.py
@@ -32,6 +32,7 @@ class Notifications(BaseDoc):
         return updated['_rev']
 
     def undo_edit(self):
+        pprint.pprint(self.doc)
         rev_list = self.get_rev_list()
         prev = self.db.get(self.doc['_id'], rev=rev_list[0])
         self.doc['user'] = prev['user']
