0cb43ea177263151a71931c7a09dbaedc6f93555
Robert Spencer <signup@robspencer.me.uk>
2015-06-17 13:43:56 +0100

    ignore deleted docs in filter

--- database/filters/devices_pox.js
+++ database/filters/devices_pox.js
@@ -1,5 +1,5 @@
 function (doc, req) {
-    if (doc.collection === 'devices' && doc.action !== '' && doc.action !== doc.state && doc.changed_by === 'user' && !doc.hasOwnProperty('hidden')) {
+    if (doc.collection === 'devices' && doc.action !== '' && doc.action !== doc.state && doc.changed_by === 'user' && !doc.hasOwnProperty('hidden') && !doc.hasOwnProperty('_deleted')) {
         return true;
     }
     return false;
