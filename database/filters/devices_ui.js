d7b3559de86552b621b99e4392c6971cc7f6fae4
Robert Spencer <signup@robspencer.me.uk>
2015-06-17 13:46:29 +0100

    ignore deleted docs in other filters

--- database/filters/devices_ui.js
+++ database/filters/devices_ui.js
@@ -1,5 +1,5 @@
 function(doc, req){
-    if(doc.collection === 'devices' && doc.action === '' && doc.state !== 'pending' && doc.changed_by === 'system' && !doc.hasOwnProperty('hidden')){
+    if(doc.collection === 'devices' && doc.action === '' && doc.state !== 'pending' && doc.changed_by === 'system' && !doc.hasOwnProperty('hidden') && !doc.hasOwnProperty('_deleted')){
         return true;
     }
     return false;
--- database/filters/edit_device.js
+++ database/filters/edit_device.js
@@ -1,5 +1,5 @@
 function(doc, req){
-    if (doc.collection === 'devices' && doc.action === '' && doc.changed_by === 'user') {
+    if (doc.collection === 'devices' && doc.action === '' && doc.changed_by === 'user' && !doc.hasOwnProperty('_deleted')) {
         return true;
     }
     return false;
