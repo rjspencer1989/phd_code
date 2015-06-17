8d9f51546be559b00c665f37ac0ca356a6f20fb1
Robert Spencer <signup@robspencer.me.uk>
2015-06-17 15:15:56 +0100

    edit device

---
+++ process_config/edit_device.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+from couchdbkit import *
+from Queue import Queue
+import threading
+import couchdb_config_parser
+import add_history
+import os
+
+db = couchdb_config_parser.get_db()
+db_info = db.info()
+
+
+class EditDeviceListener(threading.Thread):
+    def __init__(self, threadName, queue):
+        threading.Thread.__init__(self, name=threadName)
+        self.shared_object = queue
+
+    def run(self):
+        changeStream = ChangesStream(db, feed="continuous", heartbeat=True, since=db_info['update_seq'], filter='homework-remote/edit_device')
+        for change in changeStream:
+            self.shared_object.put(change)
+
+
+class EditDeviceProcessor(threading.Thread):
+    def __init__(self, threadName, queue):
+        threading.Thread.__init__(self, name=threadName)
+        self.shared_object = queue
+
+    def run(self):
+        while True:
+            change = self.shared_object.get()
+            theId = change['id']
+            theRev = change['changes'][0]['rev']
+            current_doc = db.get(theId, rev=theRev)
+            add_history.add_history_item('Edited device details', 'Edited details for %s' % (current_doc['device_name']), current_doc['doc_id'], current_doc['doc_rev'], True)
+            self.shared_object.task_done()
+
+changeQueue = Queue()
+producer = EditDeviceListener("producer", changeQueue)
+consumer = EditDeviceListener("consumer", changeQueue)
+
+if "ENV_TESTS" not in os.environ:
+    producer.start()
+    consumer.start()
--- tests/test_edit_device_metadata.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class TestEditDevice(unittest.TestCase):
-    def test_add_history_for_edit(self):
-        self.assertTrue(False)
