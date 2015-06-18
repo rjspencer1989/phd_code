d5adef587ff469f51161a7917891b4552df3534d
Robert Spencer <signup@robspencer.me.uk>
2015-06-18 12:32:43 +0100

    update filter

--- database/filters/edit_device.js
+++ database/filters/edit_device.js
@@ -1,5 +1,5 @@
 function(doc, req){
-    if (doc.collection === 'devices' && doc.action === '' && doc.changed_by === 'user' && !doc.hasOwnProperty('_deleted')) {
+    if (doc.collection === 'devices' && doc.action === '' && doc.changed_by === 'user' && doc.hasOwnProperty('_deleted') === false) {
         return true;
     }
     return false;
