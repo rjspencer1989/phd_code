e93b6a1f3a7a3402ab8007539e9cdf304f11fed6
Robert Spencer <signup@robspencer.me.uk>
2015-06-22 16:39:26 +0100

    other items

--- tests/test_wifi.py
+++ tests/test_wifi.py
@@ -33,14 +33,14 @@ class TestWifi(unittest.TestCase):
             'wpa_passphrase=whatever\n',
             'wpa_key_mgmt=WPA-PSK\n',
             'wpa_pairwise=TKIP\n',
-            'rsn_pairwise=CCMP\n',
-            'bss=wlan0_1\n',
-            'ssid=test\n',
-            'wpa=3\n',
-            'wpa_passphrase=whatever\n',
-            'wpa_key_mgmt=WPA-PSK\n',
-            'wpa_pairwise=TKIP\n',
             'rsn_pairwise=CCMP\n'
+            # 'bss=wlan0_1\n',
+            # 'ssid=test\n',
+            # 'wpa=3\n',
+            # 'wpa_passphrase=whatever\n',
+            # 'wpa_key_mgmt=WPA-PSK\n',
+            # 'wpa_pairwise=TKIP\n',
+            # 'rsn_pairwise=CCMP\n'
         ]
 
     def tearDown(self):
@@ -51,11 +51,22 @@ class TestWifi(unittest.TestCase):
         cons = edit_wifi.consumer
         cons.get_config = MagicMock(return_value={'interface': 'wlan0',
                                                   'bridge': 'br0',
-                                                  'driver': 'nl80211\n',
-                                                  'ssid': 'test_old\n',
-                                                  'hw_mode': 'g\n',
-                                                  'channel': '1\n',
-                                                  'ieee80211n': '1\n'})
+                                                  'driver': 'nl80211',
+                                                  'ssid': 'test_old',
+                                                  'hw_mode': 'g',
+                                                  'channel': '1',
+                                                  'ieee80211n': '1',
+                                                  'macaddr_acl': '0',
+                                                  'auth_algs': '1',
+                                                  'ignore_broadcast_ssid': '0',
+                                                  'eapol_key_index_workaround': '0',
+                                                  'eap_server': '0',
+                                                  'own_ip_addr': '0',
+                                                  'wpa': '3',
+                                                  'wpa_passphrase': 'whatever',
+                                                  'wpa_key_mgmt': 'WPA-PSK',
+                                                  'wpa_passphrase': 'TKIP',
+                                                  'rsn_pairwise': 'CCMP'})
         retVal = cons.generate_config(self.current_doc)
         pprint.pprint(retVal)
         self.assertListEqual(self.expected_line_list, retVal)
