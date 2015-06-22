45f7ea291b786adfdbc6cddb9127444577496183
Robert Spencer <signup@robspencer.me.uk>
2015-06-22 15:44:17 +0100

    switch expected to dict

--- tests/test_wifi.py
+++ tests/test_wifi.py
@@ -14,33 +14,33 @@ class TestWifi(unittest.TestCase):
             "password": "whatever"
         }
 
-        self.expected_line_list = [
-            'interface=wlan0\n',
-            'bridge=br0\n',
-            'driver=nl80211\n',
-            'ssid=test_old\n',
-            'hw_mode=g\n',
-            'channel=1\n',
-            'ieee80211n=1\n',
-            'macaddr_acl=0\n',
-            'auth_algs=1\n',
-            'ignore_broadcast_ssid=0\n',
-            'eapol_key_index_workaround=0\n',
-            'eap_server=0\n',
-            'own_ip_addr=127.0.0.1\n',
-            'wpa=3\n',
-            'wpa_passphrase=whatever\n',
-            'wpa_key_mgmt=WPA-PSK\n',
-            'wpa_pairwise=TKIP\n',
-            'rsn_pairwise=CCMP\n',
-            'bss=wlan0_1\n',
-            'ssid=test\n',
-            'wpa=3\n',
-            'wpa_passphrase=whatever\n',
-            'wpa_key_mgmt=WPA-PSK\n',
-            'wpa_pairwise=TKIP\n',
-            'rsn_pairwise=CCMP\n'
-        ]
+        self.expected_line_list = {
+            'interface': 'wlan0\n',
+            'bridge': 'br0\n',
+            'driver': 'nl80211\n',
+            'ssid': 'test_old\n',
+            'hw_mode': 'g\n',
+            'channel': '1\n',
+            'ieee80211n': '1\n',
+            'macaddr_acl': '0\n',
+            'auth_algs': '1\n',
+            'ignore_broadcast_ssid': '0\n',
+            'eapol_key_index_workaround': '0\n',
+            'eap_server': '0\n',
+            'own_ip_addr': '127.0.0.1\n',
+            'wpa': '3\n',
+            'wpa_passphrase': 'whatever\n',
+            'wpa_key_mgmt': 'WPA-PSK\n',
+            'wpa_pairwise': 'TKIP\n',
+            'rsn_pairwise': 'CCMP\n',
+            'bss': 'wlan0_1\n',
+            'ssid': 'test\n',
+            'wpa': '3\n',
+            'wpa_passphrase': 'whatever\n',
+            'wpa_key_mgmt': 'WPA-PSK\n',
+            'wpa_pairwise': 'TKIP\n',
+            'rsn_pairwise': 'CCMP\n'
+        }
 
     def tearDown(self):
         self.current_doc = {}
