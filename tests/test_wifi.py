50ac40441705e80edc7ffe707e9f1c4bd8489c86
Robert Spencer <signup@robspencer.me.uk>
2015-06-22 14:29:49 +0100

    simplify expected config

--- tests/test_wifi.py
+++ tests/test_wifi.py
@@ -18,22 +18,10 @@ class TestWifi(unittest.TestCase):
             'interface=wlan0\n',
             'bridge=br0\n',
             'driver=nl80211\n',
-            'logger_syslog=-1\n',
-            'logger_syslog_level=2\n',
-            'logger_stdout=-1\n',
-            'logger_stdout_level=2\n',
-            'debug=0\n',
-            'ctrl_interface=/var/run/hostapd\n',
-            'ctrl_interface_group=0\n',
             'ssid=test_old\n',
             'hw_mode=g\n',
             'channel=1\n',
             'ieee80211n=1\n',
-            'beacon_int=100\n',
-            'dtim_period=2\n',
-            'max_num_sta=255\n',
-            'rts_threshold=2347\n',
-            'fragm_threshold=2346\n',
             'macaddr_acl=0\n',
             'auth_algs=1\n',
             'ignore_broadcast_ssid=0\n',
@@ -61,6 +49,7 @@ class TestWifi(unittest.TestCase):
     def test_wifi(self):
         cons = edit_wifi.consumer
         cons.get_config = MagicMock(return_value=['interface=wlan0\n',
-                                                  'bridge=br0\n'])
+                                                  'bridge=br0\n',
+                                                  'driver='])
         retVal = cons.generate_config(self.current_doc)
         self.assertListEqual(self.expected_line_list, retVal)
