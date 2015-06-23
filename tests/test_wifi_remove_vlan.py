3467e4119a8b031dedda0e39b9fadb811d60d9ea
Robert Spencer <signup@robspencer.me.uk>
2015-06-23 16:10:32 +0100

    provide test config

--- tests/test_wifi_remove_vlan.py
+++ tests/test_wifi_remove_vlan.py
@@ -2,10 +2,57 @@ import unittest
 import remove_vlan
 from mock import MagicMock
 
+
 class TestRemoveVlan(unittest.TestCase):
     def test_remove_vlan(self):
-        keys = ['interface']
-        values = ['wlan0']
+        keys = ['interface',
+                'bridge',
+                'driver',
+                'ssid',
+                'hw_mode',
+                'channel',
+                'ieee80211n',
+                'macaddr_acl',
+                'auth_algs',
+                'ignore_broadcast_ssid',
+                'eapol_key_index_workaround',
+                'eap_server',
+                'own_ip_addr',
+                'wpa',
+                'wpa_passphrase',
+                'wpa_key_mgmt',
+                'wpa_pairwise',
+                'rsn_pairwise',
+                'bss',
+                'ssid',
+                'wpa',
+                'wpa_passphrase',
+                'wpa_key_mgmt',
+                'wpa_pairwise',
+                'rsn_pairwise']
+        values = ['wlan0',
+                  'br0',
+                  'nl80211',
+                  'test_old',
+                  'g',
+                  '1',
+                  '1',
+                  '0',
+                  '1',
+                  '0',
+                  '0',
+                  '0',
+                  '127.0.0.1',
+                  '3',
+                  'whatever',
+                  'WPA-PSK',
+                  'TKIP',
+                  'CCMP',
+                  'test',
+                  'whatever',
+                  'WPA-PSK',
+                  'TKIP',
+                  'CCMP']
         mod = remove_vlan
         mod.get_config = MagicMock(return_value=(keys, values))
         self.assertEqual((['interface'], ['wlan0']), mod.get_config())
