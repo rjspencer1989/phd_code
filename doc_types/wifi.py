f8a7fd4f321f5cd291434434d87ea4eb3dc79db3
Rob Spencer <signup@robspencer.me.uk>
2015-07-03 14:24:46 +0000

    wifi doc with typical values for ssid and wpa_passphrase

--- doc_types/wifi.py
+++ doc_types/wifi.py
@@ -1,7 +1,9 @@
 class Wifi(object):
-    def __init__(self, ssid, password, mode):
+    def __init__(self, ssid, password, mode, channel):
         self.ssid = ssid
         self.password = password
         self.mode = mode
+        self.channel = channel
         self.status = 'pending'
         self.collection = 'wifi'
+        self.encryption_type = 'wpa'
--- generate_history.py
+++ generate_history.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
-import couchdb_config_parser
+from process_config import couchdb_config_parser
+from doc_types import devices, notifications, wifi
+import random
+import string
+import json
 
 db = couchdb_config_parser.get_db()
+random.seed(1)
+ssid = "homework-%d" % (int(random.getrandbits(25)))
+password = ''.join(random.sample(string.printable, 10))
+channel = random.choice(range(1,12))
+initial_wifi = wifi.Wifi(ssid, password, 'n', channel)
+res = db.save_doc(initial_wifi.__dict__)
+print res
