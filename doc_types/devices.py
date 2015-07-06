3ba7ab9d8cdddf0f61140ac7d8f2cad5b0adf07b
Rob Spencer <signup@robspencer.me.uk>
2015-07-06 12:24:03 +0000

    create device

--- doc_types/devices.py
+++ doc_types/devices.py
@@ -1,5 +1,8 @@
+import time
+
+
 class Devices(object):
-    def __init__(self, mac_address, ip_address, device_name, user, lease_action, device_type, action):
+    def __init__(self, mac_address, ip_address, device_name, user, lease_action, device_type, action, notification_service, port):
         self._id = mac_address
         self.mac_address = mac_address
         self.ip_address = ip_address
@@ -9,3 +12,31 @@ class Devices(object):
         self.device_type = device_type
         self.action = action
         self.state = 'pending'
+        self.collection = 'devices'
+        self.host_name = device_name
+        self.notification_service = notification_service
+        self.timestamp = time.time()
+        self.port = port
+        self.connection_event = "connect"
+        self.changed_by = "system"
+        
+    def get_doc(self):
+        doc = {
+            "_id": self._id,
+            "mac_address": self.mac_address,
+            "ip_address": self.ip_address,
+            "collection": self.collection,
+            "action": self.action,
+            "device_name": self.device_name,
+            "name": self.name,
+            "lease_action": self.lease_action,
+            "host_name": self.host_name,
+            "state": self.state,
+            "device_type": self.device_type,
+            "notification_service": self.notification_service,
+            "timestamp": self.timestamp,
+            "port": self.port,
+            "connection_event": self.connection_event,
+            "changed_by": self.changed_by
+        }
+        return doc
--- generate_history.py
+++ generate_history.py
@@ -3,13 +3,17 @@ from process_config import couchdb_config_parser
 from doc_types import devices, notifications, wifi
 import random
 import string
-import json
 
 db = couchdb_config_parser.get_db()
+db.flush()
 random.seed(1)
 ssid = "homework-%d" % (int(random.getrandbits(25)))
 password = ''.join(random.sample(string.printable, 10))
 channel = random.choice(range(1,12))
 initial_wifi = wifi.Wifi(ssid, password, 'n', channel)
 res = db.save_doc(initial_wifi.__dict__)
+print res
+
+device1 = devices.Devices("aa:bb:cc:dd:ee:ff", "10.2.0.1", "device1", "user1", "add", "laptop", "permit", "email", "wlan0")
+res = db.save_doc(device1.get_doc(), force_update=True)
 print res
