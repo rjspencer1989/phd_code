240234b3f889a4843b3ca23bbfcce2932d9c1ed6
Robert Spencer <signup@robspencer.me.uk>
2015-07-16 16:42:57 +0100

    use a runner

--- process_config/perform_rollback_runner.py
+++ process_config/perform_rollback_runner.py
@@ -5,8 +5,7 @@ from couchdbkit import *
 from Queue import Queue
 import threading
 import os
-from add_history import add_history_item
-
+from perform_rollback import Rollback
 
 class RollbackListener(threading.Thread):
     def __init__(self, threadName, queue):
@@ -26,38 +25,11 @@ class RollbackProcessor(threading.Thread):
         threading.Thread.__init__(self, name=threadName)
         self.shared_object = queue
         self.db = couchdb_config_parser.get_db()
-
-    def get_events_after_timestamp(self, ts):
-        vr = self.db.view('homework-remote/undoable_events', startkey=ts)
-        return vr.all()
-
-    def get_docs_to_revert(self, timestamp):
-        events = self.get_events_after_timestamp(timestamp)
-        doc_list = {}
-        for event_val in events:
-            event = event_val['value']
-            if event['doc_id'] not in doc_list:
-                doc_list[event['doc_id']] = event
-        return doc_list
-
-    def revert(self, timestamp):
-        doc_list = self.get_docs_to_revert(timestamp)
-        for key, doc in doc_list.iteritems():
-            doc['perform_undo'] = True
-            self.db.save_doc(doc)
-
     def run(self):
         while True:
             change = self.shared_object.get()
-            if 'id' in change:
-                the_id = change['id']
-                the_rev = change['changes'][0]['rev']
-                current_doc = self.db.get(the_id, rev=the_rev)
-                self.revert(current_doc['timestamp'])
-                add_history_item("Rollback", "Roll back to %s" % (timestamp),
-                                 the_id, the_rev, False)
-                doc.status = 'done'
-                db.save_doc(current_doc['_id'])
+            roller = Rollback(self.db, change)
+            roller.rollback()
             self.shared_object.task_done()
 change_queue = Queue()
 producer = RollbackListener('producer', change_queue)
