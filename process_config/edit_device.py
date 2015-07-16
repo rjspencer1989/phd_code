e5ed745b75b03d2233d46ec53517ec727735e28d
Robert Spencer <signup@robspencer.me.uk>
2015-07-16 16:11:09 +0100

    actually commit new file

---
+++ process_config/edit_device.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from add_history import add_history_item
+
+
+def edit_device(current_doc):
+    title = 'Edited device details'
+    desc = 'Edited details for %s' % (current_doc['device_name'])
+    add_history_item(title, desc, theId, theRev, True)
