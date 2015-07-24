39932c713ea4908f1005d5c915b2659f201b60f8
Robert Spencer <signup@robspencer.me.uk>
2015-07-24 13:37:35 +0100

    use space instead of tab

--- process_config/iw_monitor.py
+++ process_config/iw_monitor.py
@@ -6,6 +6,6 @@ result = subprocess.check_output(['iw', 'dev', 'wlan0', 'station', 'dump'])
 lines = result.split('\n')
 for line in lines:
     if line.startswith('Station'):
-        fields = line.split('\t')
+        fields = line.split(' ')
         mac_addr = fields[1]
         print mac_addr
