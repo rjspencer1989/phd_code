0245cbf0be4c84074fc1e136cac7908407f0d2b0
Robert Spencer <signup@robspencer.me.uk>
2015-07-24 14:29:37 +0100

    get disconnected macs

--- process_config/iw_monitor.py
+++ process_config/iw_monitor.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 
 import subprocess
+from couchdbkit import *
+from process_config import couchdb_config_parser
+import pprint
 
+db = couchdb_config_parser.get_db()
+connected_macs = []
+keys = []
 result = subprocess.check_output(['iw', 'dev', 'wlan0', 'station', 'dump'])
 lines = result.split('\n')
 for line in lines:
     if line.startswith('Station'):
         fields = line.split(' ')
         mac_addr = fields[1]
-        print mac_addr
+        connected_macs.append(mac_addr)
+
+vr = db.view('homework-remote/dhcp')
+for item in vr.all():
+    keys.append(item['key'])
+
+disconnected = list({x for x in key_set if x not in connected})
+pprint.pprint(disconnected)
