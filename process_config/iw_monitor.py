206712d9f602f9437caaa5218a10f55887e52aad
Robert Spencer <signup@robspencer.me.uk>
2015-07-24 13:36:23 +0100

    grab mac addr

--- process_config/iw_monitor.py
+++ process_config/iw_monitor.py
@@ -4,4 +4,8 @@ import subprocess
 
 result = subprocess.check_output(['iw', 'dev', 'wlan0', 'station', 'dump'])
 lines = result.split('\n')
-print lines
+for line in lines:
+    if line.startswith('Station'):
+        fields = line.split('\t')
+        mac_addr = fields[1]
+        print mac_addr
