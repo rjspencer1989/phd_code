92dc4e227eebdc69de5cb6784e7ca0e4a16d8575
Robert Spencer <signup@robspencer.me.uk>
2015-07-27 13:28:55 +0100

    more device definitions

--- generate_history.py
+++ generate_history.py
@@ -12,6 +12,7 @@ random.seed(2)
 main_user = main_user.MainUser("", "")
 res = db.save_doc(main_user.get_doc(), force_update=True)
 print res
+
 ssid = "homework-%d" % (int(random.getrandbits(25)))
 password = ''.join(random.sample(string.ascii_lowercase + string.digits, 10))
 channel = random.choice(range(1,12))
@@ -20,20 +21,19 @@ res = db.save_doc(initial_wifi.get_doc())
 dt = datetime.datetime(2014, 01, 12, hour=10, minute=15, tzinfo=tzutc())
 add_history.add_history_item("set up wifi", "initial wifi config", res["id"], res["rev"], False, dt.isoformat())
 
-mrldesx2 = devices.Devices("a0:f4:50:f3:48:50", "10.2.0.1", "android-aa474646cd34eeaa", "", "", "add", "", "pending", "", "wlan0")
+aspire = devices.Devices("d0:27:88:80:d9:ef", "10.2.0.1", "aspire", "", "", "add", "", "pending", "", "eth1")
+res = db.save_doc(aspire.get_doc(), force_update=True)
+print res
+
+mrldesx2 = devices.Devices("a0:f4:50:f3:48:50", "10.2.0.5", "android-aa474646cd34eeaa", "", "", "add", "", "pending", "", "wlan0")
 res = db.save_doc(mrldesx2.get_doc(), force_update=True)
 print res
 
-mrldesx2.set_field("action", "permit")
-mrldesx2.set_field("changed_by", "user")
-res = db.save_doc(mrldesx2.get_doc(), force_update=True)
-print res
-
-rjsxperia1 = devices.Devices("a0:e4:53:55:00:cc", "10.2.0.33", "android-a2", "", "", "add", "", "pending", "", "wlan0")
+rjsxperia1 = devices.Devices("a0:e4:53:55:00:cc", "10.2.0.9", "android-5f251018c8685b52", "", "", "add", "", "pending", "", "wlan0")
 res = db.save_doc(rjsxperia1.get_doc(), force_update=True)
 print res
 
-mrltablet6 = devices.Devices("e0:b9:a5:8c:45:cd", "10.2.0.29", "android-a3", "", "", "add", "", "pending", "", "wlan0")
+mrltablet6 = devices.Devices("e0:b9:a5:8c:45:cd", "10.2.0.13", "android_8a0b6f3a084dc84a", "", "", "add", "", "pending", "", "wlan0")
 res = db.save_doc(mrltablet6.get_doc(), force_update=True)
 print res
 
