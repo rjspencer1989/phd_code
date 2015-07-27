fd72dd7ec875753d1bc12e4684f5e3e3ce8884ed
Robert Spencer <signup@robspencer.me.uk>
2015-07-27 14:29:40 +0100

    fix function calls

--- generate_history.py
+++ generate_history.py
@@ -20,23 +20,23 @@ res = db.save_doc(initial_wifi.get_doc())
 dt = datetime.datetime(2014, 01, 12, hour=10, minute=15, tzinfo=tzutc())
 add_history.add_history_item("set up wifi", "initial wifi config", res["id"], res["rev"], False, dt.isoformat())
 
-aspire = devices.Device("d0:27:88:80:d9:ef", "10.2.0.1", "aspire", "", "", "add", "", "pending", "", "eth1")
+aspire = devices.Device("d0:27:88:80:d9:ef", "10.2.0.1", "aspire", "eth1")
 res = db.save_doc(aspire.get_doc(), force_update=True)
 print res
 
-mrldesx2 = devices.Device("a0:f4:50:f3:48:50", "10.2.0.5", "android-aa474646cd34eeaa", "", "", "add", "", "pending", "", "wlan0")
+mrldesx2 = devices.Device("a0:f4:50:f3:48:50", "10.2.0.5", "android-aa474646cd34eeaa", "wlan0")
 res = db.save_doc(mrldesx2.get_doc(), force_update=True)
 print res
 
-rjsxperia1 = devices.Device("a0:e4:53:55:00:cc", "10.2.0.9", "android-5f251018c8685b52", "", "", "add", "", "pending", "", "wlan0")
+rjsxperia1 = devices.Device("a0:e4:53:55:00:cc", "10.2.0.9", "android-5f251018c8685b52", "wlan0")
 res = db.save_doc(rjsxperia1.get_doc(), force_update=True)
 print res
 
-mrltablet6 = devices.Device("e0:b9:a5:8c:45:cd", "10.2.0.13", "android_8a0b6f3a084dc84a", "", "", "add", "", "pending", "", "wlan0")
+mrltablet6 = devices.Device("e0:b9:a5:8c:45:cd", "10.2.0.13", "android_8a0b6f3a084dc84a", "wlan0")
 res = db.save_doc(mrltablet6.get_doc(), force_update=True)
 print res
 
-callison = devices.Device("00:13:77:e1:d2:41", "10.2.0.17", "CALLISON", "", "", "add", "", "pending", "", "eth2")
+callison = devices.Device("00:13:77:e1:d2:41", "10.2.0.17", "CALLISON", "eth2")
 res = db.save_doc(callison.get_doc(), force_update=True)
 print res
 
