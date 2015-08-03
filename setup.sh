0c67634678e673ba21fbcc036fc833509ba322e0
Robert Spencer <signup@robspencer.me.uk>
2015-08-03 13:52:30 +0100

    update setup script and make executable

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ service homework-rollback stop
 service homework-device-edit-processor stop
 service homework-ethtool-monitor stop
 service homework-iw-monitor stop
+service homework-user-edit stop
 curl http://homeuser:homework@localhost:8000/config -X DELETE
 curl http://homeuser:homework@localhost:8000/config -X PUT
 cd database && couchapp push && cd ..
@@ -19,3 +20,5 @@ service homework-rollback start
 service homework-device-edit-processor start
 service homework-ethtool-monitor start
 service homework-iw-monitor start
+service homework-user-edit-start
+
