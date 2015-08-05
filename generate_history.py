d41a7a4c58ca721e97a802367f0b45ba3e4002ee
Robert Spencer <signup@robspencer.me.uk>
2015-08-05 13:42:06 +0100

    missing comma

--- generate_history.py
+++ generate_history.py
@@ -146,7 +146,7 @@ john_twitter = notifications.Notification("John", "twitter", "rjspencer1989", ti
 res = db.save_doc(john_twitter.get_doc(), force_update=True)
 
 dt = datetime.datetime(2014, 01, 12, hour=10, minute=32, tzinfo=tzutc())
-mary_email = notifications.Notification("Mary", "email", "rob@robspencer.me.uk"timestamp=dt.isoformat())
+mary_email = notifications.Notification("Mary", "email", "rob@robspencer.me.uk", timestamp=dt.isoformat())
 res = db.save_doc(mary_email.get_doc(), force_update=True)
 
 dt = datetime.datetime(2014, 01, 12, hour=19, minute=36, tzinfo=tzutc())
