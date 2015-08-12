77ab63bad8b637b26aaddde33e654dd3563d967e
Robert Spencer <signup@robspencer.me.uk>
2015-08-12 11:45:53 +0100

    fix filenames

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ service homework-pox stop
 service homework-notifications stop
 service homework-notification-registration stop
 service homework-wifi-processor stop
-service homework-undo-processor stop
+service homework-undo stop
 service homework-rollback stop
 service homework-device-edit-processor stop
 service homework-ethtool-monitor stop
@@ -15,7 +15,7 @@ cd database && couchapp push && cd ..
 service homework-notifications start
 service homework-notification-registration start
 service homework-wifi-processor start
-service homework-undo-processor start
+service homework-undo start
 service homework-rollback start
 service homework-device-edit-processor start
 service homework-ethtool-monitor start
