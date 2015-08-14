f247721a76918f01533d00ec89279f62c2a08ca8
Robert Spencer <signup@robspencer.me.uk>
2015-08-14 12:22:54 +0100

    if no valid previous rev, delete device

--- undo/doc_types/devices.py
+++ undo/doc_types/devices.py
@@ -13,11 +13,14 @@ class Devices(BaseDoc):
 
     def undo(self):
         rev_list = self.get_rev_list()
-        doc = self.db.get(self.doc['_id'], rev=rev_list[0])
-        self.doc['device_name'] = doc['device_name']
-        self.doc['device_type'] = doc['device_type']
-        self.doc['name'] = doc['name']
-        self.doc['action'] = doc['action']
-        self.doc['changed_by'] = 'user'
+        if len(rev_list) > 0:
+            doc = self.db.get(self.doc['_id'], rev=rev_list[0])
+            self.doc['device_name'] = doc['device_name']
+            self.doc['device_type'] = doc['device_type']
+            self.doc['name'] = doc['name']
+            self.doc['action'] = doc['action']
+            self.doc['changed_by'] = 'user'
+        else:
+            self.doc['_deleted'] = True
         res = self.db.save_doc(self.doc)
         return res['rev']
