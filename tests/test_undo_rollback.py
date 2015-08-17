b371038a565a6436f709e607ad9fc2e6db439b0b
Robert Spencer <signup@robspencer.me.uk>
2015-08-17 11:57:02 +0100

    make the test fail

--- tests/test_undo_rollback.py
+++ tests/test_undo_rollback.py
@@ -2,10 +2,12 @@ import unittest
 from process_config import couchdb_config_parser
 from process_config.add_history import add_history_item
 from undo import perform_undo
+from undo.doc_types import request_revert
 import time
 import pprint
 import datetime
 
 
 class TestUndoRollback(unittest.TestCase):
-    pass
+    def test_rev_list_is_empty(self):
+        self.assertTrue(False)
