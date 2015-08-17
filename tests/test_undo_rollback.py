a820a87220b15af5f45635dde093813d9d7c2f9b
Robert Spencer <signup@robspencer.me.uk>
2015-08-17 11:38:55 +0100

    test undo rollback class added

---
+++ tests/test_undo_rollback.py
@@ -0,0 +1,11 @@
+import unittest
+from process_config import couchdb_config_parser
+from process_config.add_history import add_history_item
+from undo import perform_undo
+import time
+import pprint
+import datetime
+
+
+class TestUndoRollback(unittest.TestCase):
+    pass
