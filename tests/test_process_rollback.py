2150a65c20584c7b7fbff4584f34a09f038bc132
Robert Spencer <signup@robspencer.me.uk>
2015-08-19 16:43:46 +0100

    get rid of debug

--- tests/test_process_rollback.py
+++ tests/test_process_rollback.py
@@ -3,7 +3,6 @@ from process_config import couchdb_config_parser, perform_rollback, add_history
 import datetime
 import mock
 from dateutil.tz import tzutc
-import pprint
 
 
 class TestProcessRollback(unittest.TestCase):
@@ -93,7 +92,6 @@ class TestProcessRollback(unittest.TestCase):
 
     def test_process_rollback_get_docs_to_revert(self):
         result = self.rb.get_docs_to_revert(self.revert_doc['timestamp'])
-        pprint.pprint(result)
         self.assertEqual(2, len(result))
         k = result.keys()
         self.assertNotEqual(k[0], k[1])
