269cc540ecef943262301ab981554e0494e32367
Robert Spencer <signup@robspencer.me.uk>
2015-08-19 16:37:17 +0100

    pep8 lies

--- tests/test_undo_rollback.py
+++ tests/test_undo_rollback.py
@@ -31,7 +31,6 @@ class TestUndoRollback(unittest.TestCase):
                 cls.db.save_doc(current_doc, force_update=True)
         cls.db = None
 
-
     def setUp(self):
         self.test_doc_ids = []
         self.title = "test rollback"
