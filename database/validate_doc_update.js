76a3a8b75635dbc17478e1f7ac491a4fb1e9003b
Robert Spencer <signup@robspencer.me.uk>
2015-08-28 16:05:33 +0100

    validation error

--- database/validate_doc_update.js
+++ database/validate_doc_update.js
@@ -11,8 +11,8 @@ function (newDoc, oldDoc, userCtx){
     }
 
     function is_valid_collection(){
-        if(["wifi", "notifications", "devices", "events", "request_notification", "request_revert", "main_user"].indexOf(newDoc.collection) === -1){
-            throw({forbidden: "collection must be one of wifi, notifications, devices, events, request_notification, request_revert, main_user"});
+        if(["wifi", "notifications", "devices", "events", "request_notification", "request_revert", "main_user", "connection_state"].indexOf(newDoc.collection) === -1){
+            throw({forbidden: "collection must be one of wifi, notifications, devices, events, request_notification, request_revert, main_user, connection_state"});
         }
     }
 
