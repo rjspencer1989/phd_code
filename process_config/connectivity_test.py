a592875140e27bd2ad7e80a8eb3f66bc3470e294
Robert Spencer <signup@robspencer.me.uk>
2015-08-28 16:01:57 +0100

    fix #105

--- process_config/connectivity_test.py
+++ process_config/connectivity_test.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
 import urllib2
 from add_history import add_history_item
+import couchdb_config_parser
 
-status = False
+db = couchdb_config_parser.get_db()
+doc = db.get('connection_state')
 req = urllib2.Request('http://rjspencer1989.koding.io')
 try:
     response = urllib2.urlopen(req)
-    status = True
+    if doc['state'] != 'connected':
+        doc['state'] = 'connected'
+        res = db.save_doc(doc, force_update=True)
+        add_history_item('Internet Connection restored', 'Internet should be working again', res['id'], res['rev'], 'connection_state', undoable=False)
 except urllib2.URLError as e:
-    print e.reason
-    status = False
-    add_history_item('Internet connection error', str(e.reason), '', '', '', '', False)
+    if doc['state'] != 'disconnected':
+        doc['state'] = 'disconnected'
+        res = db.save_doc(doc, force_update=True)
+        add_history_item('Internet connection error', str(e.reason), res['id'], res['rev'], 'connection_state', undoable=False)
