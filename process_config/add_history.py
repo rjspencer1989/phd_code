2bea52fbe3e063ad5e31285d88001638accfb852
Robert Spencer <signup@robspencer.me.uk>
2015-09-01 15:21:49 +0100

    fix add history

--- process_config/add_history.py
+++ process_config/add_history.py
@@ -4,19 +4,16 @@ from dateutil.tz import tzutc
 db = couchdb_config_parser.get_db()
 
 
-def add_history_item(title, description, doc_id, doc_rev, doc_collection, action='edit', undoable=True, prompt=False, ts=None):
+def add_history_item(title, description, docs, undoable=True, prompt=False, ts=None):
     doc = {}
     doc['collection'] = 'events'
     doc['title'] = title
     doc['description'] = description
-    doc['doc_collection'] = doc_collection
-    doc['action'] = action
+    doc['docs'] = docs
     if ts is None:
         doc['timestamp'] = datetime.datetime.now(tzutc()).isoformat()
     else:
         doc['timestamp'] = ts
-    doc['doc_id'] = doc_id
-    doc['doc_rev'] = doc_rev
     doc['undoable'] = undoable
     doc['prompt'] = prompt
     doc['perform_undo'] = False
