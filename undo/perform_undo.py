17d47757086e260ac9b34552aacff253e8faacb3
Robert Spencer <signup@robspencer.me.uk>
2015-09-01 16:15:31 +0100

    should be a string

--- undo/perform_undo.py
+++ undo/perform_undo.py
@@ -4,7 +4,7 @@ from process_config import add_history, couchdb_config_parser
 db = couchdb_config_parser.get_db()
 
 def get_doc_to_undo(event):
-    if len(event[docs]) == 1:
+    if len(event['docs']) == 1:
         undo_id = event['doc_id']
         undo_doc = db.get(undo_id, revs_info=True)
         return undo_doc
