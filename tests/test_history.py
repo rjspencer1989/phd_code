916b079eba4677c57162049fc41a15a0b5b23a2e
Robert Spencer <signup@robspencer.me.uk>
2015-09-01 15:41:25 +0100

    foo

--- tests/test_history.py
+++ tests/test_history.py
@@ -1,14 +1,16 @@
 import unittest
 from process_config import add_history, couchdb_config_parser
+import pprint
 
 
 class TestHistory(unittest.TestCase):
     def test_history(self):
         doc_arr = [{'doc_id': "aabbc", 'doc_rev': "2-33aabbcc", 'doc_collection': 'wifi', 'action': 'edit'}]
-        result = add_history.add_history_item("Change WiFi", doc_arr, True)
+        result = add_history.add_history_item("Change WiFi" "change wifi", doc_arr, True)
         self.assertIsNotNone(result)
         db = couchdb_config_parser.get_db()
         doc = db.get(result['id'])
+        pprint.pprint(doc)
         self.assertIsNotNone(doc)
         self.assertEqual(doc['collection'], 'events')
         self.assertIn('timestamp', doc)
