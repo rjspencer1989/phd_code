a4a9367e68bfc292cc4b797f6eea29800889a83e
Robert Spencer <signup@robspencer.me.uk>
2015-09-14 11:23:39 +0100

    mock the get_config function

--- tests/test_process_rollback.py
+++ tests/test_process_rollback.py
@@ -1,7 +1,7 @@
 import unittest
 from process_config import couchdb_config_parser, perform_rollback, add_history
 import datetime
-import mock
+from mock import MagicMock
 from dateutil.tz import tzutc
 
 
@@ -55,6 +55,33 @@ class TestProcessRollback(unittest.TestCase):
             "timestamp": datetime.datetime(2015, 1, 20).isoformat(),
             "status": "pending"
         }
+        self.expected_line_list = [
+            'interface=wlan0\n',
+            'bridge=br0\n',
+            'driver=nl80211\n',
+            'ssid=test_old\n',
+            'hw_mode=g\n',
+            'channel=1\n',
+            'ieee80211n=1\n',
+            'macaddr_acl=0\n',
+            'auth_algs=1\n',
+            'ignore_broadcast_ssid=0\n',
+            'eapol_key_index_workaround=0\n',
+            'eap_server=0\n',
+            'own_ip_addr=127.0.0.1\n',
+            'wpa=3\n',
+            'wpa_passphrase=whatever\n',
+            'wpa_key_mgmt=WPA-PSK\n',
+            'wpa_pairwise=TKIP\n',
+            'rsn_pairwise=CCMP\n',
+            'bss=wlan0_1\n',
+            'ssid=test\n',
+            'wpa=3\n',
+            'wpa_passphrase=whatever\n',
+            'wpa_key_mgmt=WPA-PSK\n',
+            'wpa_pairwise=TKIP\n',
+            'rsn_pairwise=CCMP'
+        ]
         res1 = self.db.save_doc(self.wifi_doc)
         self.test_doc_ids.append(res1['id'])
         dt = datetime.datetime(2015, 1, 5, hour=10, minute=5)
@@ -84,6 +111,7 @@ class TestProcessRollback(unittest.TestCase):
         self.rb = perform_rollback.Rollback(self.db, self.revert_doc)
 
     def tearDown(self):
+        self.expected_line_list = []
         for doc in self.test_doc_ids:
             opened = self.db.get(doc)
             opened['_deleted'] = True
@@ -101,5 +129,43 @@ class TestProcessRollback(unittest.TestCase):
         self.assertNotEqual(k[0], k[1])
 
     def test_rollback(self):
+        cons = edit_wifi
+        keys = ['interface',
+                'bridge',
+                'driver',
+                'ssid',
+                'hw_mode',
+                'channel',
+                'ieee80211n',
+                'macaddr_acl',
+                'auth_algs',
+                'ignore_broadcast_ssid',
+                'eapol_key_index_workaround',
+                'eap_server',
+                'own_ip_addr',
+                'wpa',
+                'wpa_passphrase',
+                'wpa_key_mgmt',
+                'wpa_pairwise',
+                'rsn_pairwise']
+        values = ['wlan0',
+                  'br0',
+                  'nl80211',
+                  'test_old',
+                  'g',
+                  '1',
+                  '1',
+                  '0',
+                  '1',
+                  '0',
+                  '0',
+                  '0',
+                  '127.0.0.1',
+                  '3',
+                  'whatever',
+                  'WPA-PSK',
+                  'TKIP',
+                  'CCMP']
+        cons.get_config = MagicMock(return_value=(keys, values))
         result = self.rb.revert(self.revert_doc['timestamp'])
         self.assertTrue(result)
