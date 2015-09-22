1c92b0a2afea6d48560347470da576f51eae47ce
Robert Spencer <signup@robspencer.me.uk>
2015-09-22 11:59:21 +0100

    find config path

--- process_config/couchdb_config_parser.py
+++ process_config/couchdb_config_parser.py
@@ -10,6 +10,7 @@ def get_db():
                                         "DB": "config",
                                         "SERVER_NAME": "localhost"})
     path = "%s/couchdb.conf" % (expanduser('~'))
+    print path
     config.read(path)
     user = config.get('DEFAULT', 'ADMIN')
     password = config.get('DEFAULT', 'ADMIN_PASSWORD')
