0279ba037497d107d2b5fcad24c661759995a9aa
Robert Spencer <signup@robspencer.me.uk>
2015-11-10 15:32:41 +0000

    debug

--- generate_history.py
+++ generate_history.py
@@ -6,6 +6,7 @@ import string
 import datetime
 from dateutil.tz import tzutc
 import subprocess
+import pprint
 
 db = couchdb_config_parser.get_db()
 main_user = main_user.MainUser("", "")
@@ -68,6 +69,7 @@ mrldesx2.set_field("notification_service", "email")
 mrldesx2.set_field("name", "Mary")
 mrldesx2.set_field("device_type", "phone")
 mrldesx2.set_field("state", "permit")
+pprint.pprint(mrldesx2)
 res = db.save_doc(mrldesx2.get_doc(), force_update=True)
 dt = datetime.datetime(2014, 01, 12, hour=11, minute=1, tzinfo=tzutc())
 doc_arr = [{'doc_id': res['id'], 'doc_rev': res['rev'], 'doc_collection': 'devices', 'action': 'edit'}]
