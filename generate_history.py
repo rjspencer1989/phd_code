ccff7f37ec5fecd2758f309b7d822b5084e32781
Rob Spencer <signup@robspencer.me.uk>
2015-11-13 13:40:15 +0000

    forgot to save the doc

--- generate_history.py
+++ generate_history.py
@@ -108,6 +108,7 @@ xbox.set_field("notification_service", "phone")
 xbox.set_field("name", "John")
 xbox.set_field("device_type", "other")
 xbox.set_field("state", "permit")
+res = db.save_doc(xbox.get_doc(), force_update=True)
 dt = datetime.datetime(2014, 07, 25, hour=18, minute=25, tzinfo=tzutc())
 doc_arr = [{'doc_id': res['id'], 'doc_rev': res['rev'], 'doc_collection': 'devices', 'action': 'edit'}]
 add_history.add_history_item("Device Permitted", "XBOX One was permitted to access your network", doc_arr, undoable=True, prompt=True, ts=dt.isoformat())
