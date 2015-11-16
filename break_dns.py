2a11cfe114745e2ed5511a5f4f45d4a73d3ece27
Robert Spencer <signup@robspencer.me.uk>
2015-11-16 15:38:09 +0000

    break dns

--- break_dns.py
+++ break_dns.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 from couchdbkit import *
 from process_config import couchdb_config_parser
+import subprocess
 
 db = couchdb_config_parser.get_db()
-print db
+
+lines = []
+with open("/etc/dnsmasq.conf", "r") as dh:
+    lines = dh.readlines()
+
+if len(lines) > 0:
+    lines.append("no-resolv\n")
+    with open("/etc/dnsmasq.conf", "w") as dhw:
+        dhw.writelines(lines)
+
+cmd = ["service", "dnsmasq", "restart"]
+subprocess.call(cmd)
