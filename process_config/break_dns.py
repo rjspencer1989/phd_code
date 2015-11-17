a4b16000a37fb749f489b20a602f87d0f18d573e
Robert Spencer <signup@robspencer.me.uk>
2015-11-17 16:11:24 +0000

    get rid of main user

--- process_config/break_dns.py
+++ process_config/break_dns.py
@@ -8,9 +8,9 @@ db = couchdb_config_parser.get_db()
 
 def break_dns(current_doc):
     title = 'DNS settings updated'
-    desc = 'You are now using Google\'s DNS servers.
+    desc = "You are now using Google's DNS servers."
     ts = current_doc['event_timestamp'] if 'event_timestamp' in current_doc else None
-    add_history_item(title, desc, current_doc['_id'], current_doc['_rev'], 'main_user', 'edit', True, ts=ts)
+    add_history_item(title, desc, current_doc['_id'], current_doc['_rev'], 'dns', 'edit', True, ts=ts)
     if 'event_timestamp' in current_doc:
         del current_doc['event_timestamp']
         db.save_doc(current_doc, force_update=True)
