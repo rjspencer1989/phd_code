05015a20f2cb5ced597825350d53bdb34e3a533d
Robert Spencer <signup@robspencer.me.uk>
2015-11-18 14:10:39 +0000

    make that undoable

--- process_config/break_dns.py
+++ process_config/break_dns.py
@@ -11,7 +11,7 @@ def break_dns(current_doc):
     desc = "You are now using Google's DNS servers."
     ts = current_doc['event_timestamp'] if 'event_timestamp' in current_doc else None
     doc_arr = [{'doc_id': current_doc['_id'], 'doc_rev': current_doc['_rev'], 'doc_collection': 'dns', 'action': 'edit'}]
-    add_history_item(title, desc, doc_arr, 'edit', True, ts=ts)
+    add_history_item(title, desc, doc_arr, 'edit', undoable=True, ts=ts)
     if 'event_timestamp' in current_doc:
         del current_doc['event_timestamp']
         db.save_doc(current_doc, force_update=True)
