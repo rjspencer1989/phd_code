e15ffa7918aa55cbfc8ac9a18243978011963a0d
Robert Spencer <signup@robspencer.me.uk>
2015-11-18 14:12:58 +0000

    make setup script include breaking dns

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ service homework-device-edit-processor stop
 service homework-ethtool-monitor stop
 service homework-iw-monitor stop
 service homework-user-edit stop
+service homework-break-dns stop
 curl http://admin:homework@localhost:8000/config -X DELETE
 curl http://admin:homework@localhost:8000/config -X PUT
 cd database && couchapp push && cd ..
@@ -28,4 +29,5 @@ service homework-device-edit-processor start
 service homework-ethtool-monitor start
 service homework-iw-monitor start
 service homework-user-edit start
+service homework-break-dns start
 service homework-pox start
