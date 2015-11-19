530e4a99a7dae1ff0a58c77d77e1b59239bb70de
Robert Spencer <signup@robspencer.me.uk>
2015-11-19 13:33:15 +0000

    break DNS

---
+++ process_config/break_dns.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from add_history import add_history_item
+import couchdb_config_parser
+
+db = couchdb_config_parser.get_db()
+
+
+def break_dns(current_doc):
+    title = 'Internet Connection Error'
+    desc = "An error occurred when requesting DNS lookups. Check your settings."
+    ts = current_doc['event_timestamp'] if 'event_timestamp' in current_doc else None
+    doc_arr = [{'doc_id': current_doc['_id'], 'doc_rev': current_doc['_rev'], 'doc_collection': 'dns', 'action': 'edit'}]
+    add_history_item(title, desc, doc_arr, undoable=False, ts=ts)
+    if 'event_timestamp' in current_doc:
+        del current_doc['event_timestamp']
+        current_doc['status'] = 'done'
+        db.save_doc(current_doc, force_update=True)
