aaff29b88cfa77aeeecf2696b8e409ad59a50b1d
Robert Spencer <signup@robspencer.me.uk>
2015-11-19 13:02:52 +0000

    update setup script

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ service homework-device-edit-processor stop
 service homework-ethtool-monitor stop
 service homework-iw-monitor stop
 service homework-user-edit stop
-service homework-break-dns stop
+service homework-set-dns stop
 curl http://admin:homework@localhost:8000/config -X DELETE
 curl http://admin:homework@localhost:8000/config -X PUT
 cd database && couchapp push && cd ..
@@ -29,5 +29,5 @@ service homework-device-edit-processor start
 service homework-ethtool-monitor start
 service homework-iw-monitor start
 service homework-user-edit start
-service homework-break-dns start
+service homework-set-dns start
 service homework-pox start
