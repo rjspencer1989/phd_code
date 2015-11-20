3992276f5e8f8a9eaaa958af411bf943e2e39539
Robert Spencer <signup@robspencer.me.uk>
2015-11-20 10:56:54 +0000

    undo

--- undo/doc_types/dns.py
+++ undo/doc_types/dns.py
@@ -3,9 +3,19 @@ from base_doc import BaseDoc
 class Dns(BaseDoc):
     def get_rev_list(self):
         rl = super(Dns, self)
-        return rl
+        rev_list = []
+        for rev in rl:
+            doc = self.db.get(self.doc['_id'], rev=rev)
+            if doc['status'] == "done":
+                rev_list.append(rev)
+        return rev_list
 
     def undo(self):
         rev_list = self.get_rev_list()
+        if len(rev_list) == 0:
+            return None
+        rev = rev_list[0]
+        doc = self.db.get(self.doc['_id'], rev=rev)
+        self.doc = doc
         res = self.db.save_doc(self.doc, force_update=True)
         return res
