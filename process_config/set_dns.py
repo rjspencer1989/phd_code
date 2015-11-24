c8419bfefb5e753296688644fb23ef28ed7e798c
Robert Spencer <signup@robspencer.me.uk>
2015-11-24 12:44:05 +0000

    make undo of dns change not undoable

--- process_config/set_dns.py
+++ process_config/set_dns.py
@@ -9,12 +9,14 @@ db = couchdb_config_parser.get_db()
 def set_dns(current_doc, from_undo=False):
     title = 'DNS settings updated'
     desc = "You are now using Google's DNS servers."
+    undoable = True
     if from_undo is True:
         title = "Undo DNS change"
         desc = "DNS configuration has been reverted."
+        undoable = False
     ts = current_doc['event_timestamp'] if 'event_timestamp' in current_doc else None
     doc_arr = [{'doc_id': current_doc['_id'], 'doc_rev': current_doc['_rev'], 'doc_collection': 'dns', 'action': 'edit'}]
-    add_history_item(title, desc, doc_arr, undoable=True, ts=ts)
+    add_history_item(title, desc, doc_arr, undoable=undoable, ts=ts)
     if 'event_timestamp' in current_doc:
         del current_doc['event_timestamp']
         current_doc['status'] = 'done'
