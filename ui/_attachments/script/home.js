9d5471a4f4ace222fb35b4589228b44fcb852614
Rob Spencer <signup@robspencer.me.uk>
2015-11-25 14:33:49 +0000

    add notifications to home

--- ui/_attachments/script/home.js
+++ ui/_attachments/script/home.js
@@ -18,6 +18,53 @@ window.App.Views.WifiHome = Backbone.View.extend({
     }
 });
 
+window.App.Views.NotificationHome = Backbone.View.extend({
+    tagName: "tr",
+    template: window.JST.home_notification,
+    render: function(){
+        "use strict";
+        this.$el.empty().append(this.template(this.model.toJSON()));
+        return this;
+    }
+});
+
+window.App.Views.NotificationsHome = Backbone.View.extend({
+    el: "#home-notifications",
+    template: window.JST.home_notifications,
+    collection: new App.Collections.Notifications(),
+    initialize: function(){
+        "use strict";
+        this.listenTo(this.collection, "reset", this.render);
+        this.listenTo(this.collection, "change", this.render);
+        this.subviews = [];
+        this.collection.fetch({
+            reset: true
+        });
+    },
+    
+    addOne: function(notification){
+        "use strict";
+        var view = new window.App.Views.NotificationHome({model: notification});
+        this.subviews.push(view);
+        this.$("tbody").append(view.render().el);
+    },
+
+    render: function () {
+        "use strict";
+        this.$el.empty().append(this.template());
+        this.collection.each(this.addOne, this);
+        return this;
+    },
+
+    exit: function(){
+        "use strict";
+        for (var index in this.subviews) {
+            this.subviews[index].remove();
+        }
+        this.remove();
+    }
+});
+
 window.App.Views.DeviceHome = Backbone.View.extend({
     tagName: "tr",
     template: window.JST.home_device,
@@ -74,6 +121,7 @@ window.App.Views.Home = Backbone.View.extend({
         this.render();
         this.wifi_view = new window.App.Views.WifiHome();
         this.devices_view = new window.App.Views.ConnectedDevicesHome();
+        this.notifications_view = new window.App.Views.NotificationsHome();
     },
 
     render: function () {
@@ -87,6 +135,7 @@ window.App.Views.Home = Backbone.View.extend({
         "use strict";
         this.wifi_view.remove();
         this.devices_view.exit();
+        this.notifications_view.exit();
         this.remove();
     }
 });
