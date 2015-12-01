4e268f48ad92e544b815edcb4fd24db1709c9cea
Rob Spencer <signup@robspencer.me.uk>
2015-12-01 14:03:42 +0000

    restore save_wifi with cached UI

--- ui/_attachments/script/wifi.js
+++ ui/_attachments/script/wifi.js
@@ -27,10 +27,62 @@ WifiModel = Marionette.ItemView.extend({
         "submit #save-wifi-form": "saveWifi"
     },
     
-   onShow: function(){
+    onShow: function(){
         window.setActiveLink("wifi-link");
         $('[data-toggle="tooltip"]').tooltip();
-   }
+    },
+    
+    ui: {
+        ssid: '#ssid_input',
+        channel: '#channel_select : selected',
+        pasword: '#password_input',
+        mode: '#mode_select: selected'
+    },
+    
+    saveWifi: function(event){
+        "use strict";
+        event.preventDefault();
+        var changed = false;
+        var newSSID = ui.ssid.val();
+        var newChannel = ui.channel.val();
+        var newPassword = ui.pasword.val();
+        var newMode = ui.mode.val();
+        var mod = this.collection.at(0);
+        if(newSSID !== ""){
+            changed = true;
+            mod.set({ssid: newSSID});
+        }
+        if(newChannel !== "blank"){
+            changed = true;
+            mod.set({channel: newChannel});
+        }
+        if(newMode !== "blank"){
+            changed = true;
+            mod.set({mode: newMode});
+        }
+        if(newPassword !== ""){
+            changed = true;
+            mod.set({password: newPassword});
+        }
+        mod.set({status: "pending"});
+
+        if (newSSID === "" && newPassword === "") {
+            mod.set({with_bss: false});
+        }
+        if(changed === true){
+            mod.save(null, {
+                error: function(model, response){
+                    $(".alert.alert-danger").append(response.reason).show();
+                    this.collection.fetch({reset: true});
+                },
+                success: function(model, response){
+                    $(".alert.alert-success").show();
+                }
+            });
+        }
+        console.log(changed);
+        return false;
+    }
 });
 
 WiFi = Marionette.CollectionView.extend({
