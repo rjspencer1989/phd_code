e917dd02af6e2ab2de0ea14d0d068fa1037dbf7d
Rob Spencer <signup@robspencer.me.uk>
2015-12-14 20:32:27 +0000

    make the ui refresh when there is a new device

--- database/filters/devices_ui.js
+++ database/filters/devices_ui.js
@@ -1,5 +1,5 @@
 function(doc, req){
-    if(doc.collection === 'devices' && doc.action === '' && doc.state !== 'pending' && doc.changed_by === 'system' && !doc.hasOwnProperty('hidden') && !doc.hasOwnProperty('_deleted')){
+    if(doc.collection === 'devices' && doc.action === '' && doc.changed_by === 'system' && !doc.hasOwnProperty('hidden') && !doc.hasOwnProperty('_deleted')){
         return true;
     }
     return false;
