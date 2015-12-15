4f7130b5948a7b3536c3e23a363713272bb15488
Robert Spencer <signup@robspencer.me.uk>
2015-12-15 16:12:18 +0000

    make it executable by self and as import

--- process_config/reset.py
+++ process_config/reset.py
@@ -2,9 +2,14 @@
 import subprocess
 import time
 import generate_history
-cmd = ["./setup.sh"]
-subprocess.call(cmd, cwd="..")
-print 'reset'
-time.sleep(5)
-print 'generating history'
-generate_history.generate()
+
+def reset():
+    cmd = ["./setup.sh"]
+    subprocess.call(cmd, cwd="..")
+    print 'reset'
+    time.sleep(5)
+    print 'generating history'
+    generate_history.generate()
+
+if __name__ == "__main__":
+    reset()
