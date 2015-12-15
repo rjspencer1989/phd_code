ea85e9fa85488569cca124d7450f517687425f03
Robert Spencer <signup@robspencer.me.uk>
2015-12-15 15:05:24 +0000

    set working directory

--- process_config/reset.py
+++ process_config/reset.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import subprocess
 import generate_history
-cmd = ["../setup.sh"]
-subprocess.call(cmd)
+cmd = ["./setup.sh"]
+subprocess.call(cmd, cwd="..")
 generate_history
