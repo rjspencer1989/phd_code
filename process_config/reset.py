234e0e840cc30eed7de4c1350a3981802b2e6334
Robert Spencer <signup@robspencer.me.uk>
2015-12-15 16:03:48 +0000

    sleep

--- process_config/reset.py
+++ process_config/reset.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import subprocess
+import time
 import generate_history
 cmd = ["./setup.sh"]
 subprocess.call(cmd, cwd="..")
-# generate_history.generate()
+print 'reset'
+time.sleep(5)
+print 'generating history'
+generate_history.generate()
